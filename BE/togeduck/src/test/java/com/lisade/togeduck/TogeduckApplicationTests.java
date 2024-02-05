4ef78e90c6b52855353909eaa6c3b3602da41ea2
Hymin2 <khm8246@gmail.com>
2024-02-05 13:52:47 +0900

    chore: 프로젝트 생성 및 초기 설정 #2
    - 구현 내용
        1. Spring Boot 프로젝트 생성
        2. Dependencies 추가
        3. .gitignore 설정

---
+++ BE/togeduck/src/test/java/com/lisade/togeduck/TogeduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisade.togeduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogeduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
