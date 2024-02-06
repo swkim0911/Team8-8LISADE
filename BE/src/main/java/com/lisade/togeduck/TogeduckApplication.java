3397ad687490a205f23198ce6d300e601eac3f10
Hymin2 <khm8246@gmail.com>
2024-02-06 17:46:10 +0900

    Chore: google style format 적용

--- BE/src/main/java/com/lisade/togeduck/TogeduckApplication.java
+++ BE/src/main/java/com/lisade/togeduck/TogeduckApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TogeduckApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TogeduckApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TogeduckApplication.class, args);
+    }
 
 }
--- BE/src/test/java/com/lisade/togeduck/TogeduckApplicationTests.java
+++ BE/src/test/java/com/lisade/togeduck/TogeduckApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TogeduckApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
