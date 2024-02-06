54dec867fc6addcc375326792636d56fd28156c3
Hymin2 <khm8246@gmail.com>
2024-02-06 23:47:56 +0900

    feat: User Entity 생성 #13
    - 구현 내용
        1. User Entity 생성
        2. ERD를 바탕으로 필드 생성
        3. Authority enum 생성

---
+++ BE/src/main/java/com/lisade/togeduck/entity/User.java
@@ -0,0 +1,44 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private enum AuthorityType {
+        ROLE_USER, ROLE_ADMIN
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, length = 20)
+    private String userId;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "nickname", nullable = false, length = 20)
+    private String nickname;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "authorization", nullable = false)
+    private AuthorityType authority;
+}
