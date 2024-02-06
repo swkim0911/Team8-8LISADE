1ea57604f5c041c551850afc350511af26db9b51
Hymin2 <khm8246@gmail.com>
2024-02-07 00:45:54 +0900

    feat: Bus Entity 생성 #13
    - 구현 내용
        1. Bus Entity를 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/entity/Bus.java
@@ -0,0 +1,34 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bus {
+
+    private enum BusType {
+        SMALL, MEDIUM, BIG
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "type", nullable = false)
+    private BusType type;
+}
