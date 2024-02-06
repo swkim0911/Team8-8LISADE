fcf48d0b64d0f76030bd9c4e07a1bf2627400fee
Hymin2 <khm8246@gmail.com>
2024-02-07 00:04:44 +0900

    feat: BaseEntity 필드 수정#13
    - 구현 내용
        1. BaseEntity의 필드에 @Column Annotation 추가
        2. createdAt은 수정을 허용하지 않고, updatedAt은 수정을 허용

--- BE/src/main/java/com/lisade/togeduck/entity/BaseEntity.java
+++ BE/src/main/java/com/lisade/togeduck/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.lisade.togeduck.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -14,8 +15,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(updatable = true)
     private LocalDateTime updatedAt;
 }
