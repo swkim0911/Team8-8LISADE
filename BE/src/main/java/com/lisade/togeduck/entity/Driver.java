bc08866c86ea46f5ca574240574782d8a3ccd6c4
Hymin2 <khm8246@gmail.com>
2024-02-07 00:36:15 +0900

    feat: Driver Entity 생성 #13
    - 구현 내용
        1. Driver Entity를 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/entity/Driver.java
@@ -0,0 +1,36 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Driver extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "company", nullable = false)
+    private String company;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+}
