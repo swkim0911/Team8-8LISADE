5ec86001c2237a2743a431f316b2c1158fb58cb3
Hymin2 <khm8246@gmail.com>
2024-02-07 00:17:56 +0900

    feat: EnableJpaAuting Annotaion 추가 #13
    - 구현 내용
        1. @CreatedDate와 @LastModifiedDate를 사용하기 위해 TogeduckApplication에 @EnableJpaAuting를 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/TogeduckApplication.java
+++ BE/src/main/java/com/lisade/togeduck/TogeduckApplication.java
@@ -2,12 +2,14 @@ package com.lisade.togeduck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TogeduckApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TogeduckApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TogeduckApplication.class, args);
+    }
 
 }
