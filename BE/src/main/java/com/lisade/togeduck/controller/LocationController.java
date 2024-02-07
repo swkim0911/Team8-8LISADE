3cc8f1525d4fbc351675366c7898c90bb41a8167
Hymin2 <khm8246@gmail.com>
2024-02-08 01:43:14 +0900

    Refactor: LocationController의 getList 함수의 return 타입 변경
    - 구현 내용
        1. 기존 ResponseEntity<Object> 타입으로 리턴한 것을 LocationListDto타입으로 리턴하도록 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/controller/LocationController.java
+++ BE/src/main/java/com/lisade/togeduck/controller/LocationController.java
@@ -1,8 +1,8 @@
 package com.lisade.togeduck.controller;
 
+import com.lisade.togeduck.dto.response.LocationListDto;
 import com.lisade.togeduck.service.LocationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ public class LocationController {
     private final LocationService locationService;
 
     @GetMapping
-    public ResponseEntity<Object> getList() {
-        return ResponseEntity.ok(locationService.getList());
+    public LocationListDto getList() {
+        return locationService.getList();
     }
 }
