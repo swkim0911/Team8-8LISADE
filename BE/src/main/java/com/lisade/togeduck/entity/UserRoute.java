e7f093210bd94fb706f35765eff8234e13299a4c
Hymin2 <khm8246@gmail.com>
2024-02-07 10:23:30 +0900

    Feat: UserRoute Entity 생성 #13
    - 구현 내용
        1. UserRoute Entity를 생성했습니다.
        2. BaseEntity를 상속받았습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/entity/UserRoute.java
@@ -0,0 +1,27 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserRoute extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+}
