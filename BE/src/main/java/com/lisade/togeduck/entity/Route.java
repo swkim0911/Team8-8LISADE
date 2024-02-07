b27ae67a469eed548d666cbdb57c0aed1275159c
Hymin2 <khm8246@gmail.com>
2024-02-07 10:22:04 +0900

    Feat: Route Entity 생성 #13
    - 구현 내용
        1. Route Entity를 생성했습니다.
        2. BaseEntity를 상속받았습니다.
        3. RouteStatus enum을 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/entity/Route.java
@@ -0,0 +1,51 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Route extends BaseEntity {
+
+    private enum RouteStatus {
+        PROGRESS, COMPLETE
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RouteStatus status;
+
+    @Column(name = "statred_at", nullable = false)
+    private LocalDateTime startedAt;
+
+    @Column(name = "expected_time", nullable = false)
+    private LocalTime expectedTime;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "distance", nullable = false)
+    private Integer distance;
+}
