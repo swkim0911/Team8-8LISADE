1caeaeb2fcf1fb2e54335d6a72392446d2121b9b
Hymin2 <khm8246@gmail.com>
2024-02-07 10:21:23 +0900

    Feat: FestivalImage Entity 생성 #13
    - 구현 내용
        1. FestivalImage Entity를 생성했습니다.
        2. BaseEntity를 상속받았습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/entity/FestivalImage.java
@@ -0,0 +1,30 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FestivalImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "path", nullable = false, length = 200)
+    private String path;
+}
