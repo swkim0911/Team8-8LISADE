0faf37af05dc9fd99b510c3e29c84dc8f06663ae
Hymin2 <khm8246@gmail.com>
2024-02-07 10:58:41 +0900

    Feat: Route와 Station 관계 생성 #13
    - 구현 내용
        1. Route Entity와 Station Entity의 관계를 생성했습니다.
        2. FetchType을 LAZY로 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/entity/Route.java
+++ BE/src/main/java/com/lisade/togeduck/entity/Route.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -54,6 +55,10 @@ public class Route extends BaseEntity {
     @Column(name = "distance", nullable = false)
     private Integer distance;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "station_id", nullable = false)
+    private Station station;
+    
     @OneToMany(mappedBy = "route")
     private List<Seat> seats;
 
