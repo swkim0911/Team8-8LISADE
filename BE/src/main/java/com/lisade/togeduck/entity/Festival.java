adfe04dd4e82cd1d688c38e02060912d728bfe3b
Hymin2 <khm8246@gmail.com>
2024-02-07 10:35:45 +0900

    Feat: Festival와 FestivalImage 관계 생성 #13
    - 구현 내용
        1. Festival Entity와 FestivalImage Entity의 관계를 생성했습니다.
        2. FestivalImage의 FetchType을 LAZY로 변경했습니다.
        3. CascadeType을 ALL로 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/entity/Festival.java
+++ BE/src/main/java/com/lisade/togeduck/entity/Festival.java
@@ -1,5 +1,6 @@
 package com.lisade.togeduck.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,8 +8,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -53,4 +56,7 @@ public class Festival extends BaseEntity {
 
     @Column(name = "started_at", nullable = false)
     private LocalDateTime startedAt;
+
+    @OneToMany(mappedBy = "festival", cascade = CascadeType.ALL)
+    private List<FestivalImage> festivalImages;
 }
--- BE/src/main/java/com/lisade/togeduck/entity/FestivalImage.java
+++ BE/src/main/java/com/lisade/togeduck/entity/FestivalImage.java
@@ -2,9 +2,12 @@ package com.lisade.togeduck.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -27,4 +30,8 @@ public class FestivalImage extends BaseEntity {
 
     @Column(name = "path", nullable = false, length = 200)
     private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "festival_id", nullable = false)
+    private Festival festival;
 }
