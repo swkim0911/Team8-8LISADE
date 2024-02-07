b38813f26b1d60982cb696f46de4f5a3e6f2adf7
Hymin2 <khm8246@gmail.com>
2024-02-07 11:57:14 +0900

    Feat: Bus Entity에서 @Enumerated 추가 #13
    - 구현 내용
        1. Bus Entity에서 type을 string 값으로 저장하기 위해 @Enumerated(value = EnumType.STRING)를 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/entity/Bus.java
+++ BE/src/main/java/com/lisade/togeduck/entity/Bus.java
@@ -3,6 +3,8 @@ package com.lisade.togeduck.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,6 +37,7 @@ public class Bus {
     private Long id;
 
     @Column(name = "type", nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private BusType type;
 
     @Column(name = "number_of_seats", nullable = false)
