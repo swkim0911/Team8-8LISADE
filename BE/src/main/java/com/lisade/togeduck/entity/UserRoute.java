26e6ea3b31d3d4615a2db0176b4c56d8857d71b7
Hymin2 <khm8246@gmail.com>
2024-02-07 11:05:13 +0900

    Feat: Route와 UserRoute 관계 생성 #13
    - 구현 내용
        1. Route Entity와 UserRoute Entity의 관계를 생성했습니다.
        2. FetchType을 LAZY로 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/entity/UserRoute.java
+++ BE/src/main/java/com/lisade/togeduck/entity/UserRoute.java
@@ -2,9 +2,12 @@ package com.lisade.togeduck.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -24,4 +27,8 @@ public class UserRoute extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "route_id", nullable = false)
+    private Route route;
 }
