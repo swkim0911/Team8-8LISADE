25ea283a2596cf7d2a9c37dd94c1fb1d061387e8
YUJEONG YUN <yunyj99@naver.com>
2024-02-07 18:39:09 +0900

    Refactor: 공통 바텀네비게이션 화면 전환 동작 적용#11

--- AOS/app/src/main/java/com/softeer/togeduck/MainActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/MainActivity.kt
@@ -2,10 +2,27 @@ package com.softeer.togeduck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.softeer.togeduck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val viewBottomNavigation = binding.bottomNav
+        viewBottomNavigation.setupWithNavController(navController)
     }
 }
