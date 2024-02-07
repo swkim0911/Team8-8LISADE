4d524e3a8148c06d7a9a209e07863ddc556893e7
Hymin2 <khm8246@gmail.com>
2024-02-07 10:23:55 +0900

    Feat: Station Entity 생성 #13
    - 구현 내용
        1. Station Entity를 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/entity/Station.java
@@ -0,0 +1,36 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "x_pos", nullable = false)
+    private Double xPos;
+
+    @Column(name = "y_pos", nullable = false)
+    private Double yPos;
+}
