ad9bcc891e941c808dbf7977757c463a19ca9325
dlwnsgus07 <dlwnsgus07@naver.com>
2024-02-08 18:30:25 +0900

    Feat : 행사 목록 데이터베이스 조회 기능 추가
    데이터베이스에 접근하여 특정 카테고리의 행사 목록을 페이징하여 return 하는 기능 추가

--- BE/src/main/java/com/lisade/togeduck/repository/FestivalRepository.java
+++ BE/src/main/java/com/lisade/togeduck/repository/FestivalRepository.java
@@ -1,8 +1,14 @@
 package com.lisade.togeduck.repository;
 
+import com.lisade.togeduck.entity.Category;
 import com.lisade.togeduck.entity.Festival;
+import com.lisade.togeduck.entity.Status;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long> {
 
+    Slice<Festival> findSliceByCategoryAndStatus(final Pageable pageable, Category category,
+        Status status);
 }
