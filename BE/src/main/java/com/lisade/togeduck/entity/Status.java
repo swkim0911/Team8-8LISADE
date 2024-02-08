6f48f9609d62ca7a2e92c8013975a86b8517d10c
dlwnsgus07 <dlwnsgus07@naver.com>
2024-02-08 18:25:02 +0900

    Feat : code에 해당하는 Status를 찾는 기능 추가
    클라이언트로부터 요청을 받을때 PathVariable로 받는 status의 code정보 값을 통해 해당하는 Status를 찾아 return하고 없을시 Exception을 던지도록 제작

--- BE/src/main/java/com/lisade/togeduck/entity/Status.java
+++ BE/src/main/java/com/lisade/togeduck/entity/Status.java
@@ -1,5 +1,25 @@
 package com.lisade.togeduck.entity;
 
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
 public enum Status {
-    RECRUITMENT, TERMINATION
+    RECRUITMENT("1"), TERMINATION("2");
+
+    private final String code;
+
+    Status(String code) {
+        this.code = code;
+    }
+
+    public static Status of(String code) {
+        if (code == null) {
+            throw new IllegalArgumentException("코드가 없습니다.");
+        }
+        return Arrays.stream(Status.values())
+            .filter(category -> category.getCode().equals(code))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("일치하는 Status 코드가 없습니다."));
+    }
 }
