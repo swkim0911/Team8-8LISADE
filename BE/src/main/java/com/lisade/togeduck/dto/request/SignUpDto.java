8a342f6d7c00333d9a433dc9a34fb0fae427b6e5
swkim0911 <kimsungwon364@gmail.com>
2024-02-09 21:17:52 +0900

    Feat: SignUpDto 클래스 생성 회원가입 dto 생성

---
+++ BE/src/main/java/com/lisade/togeduck/dto/request/SignUpDto.java
@@ -0,0 +1,9 @@
+package com.lisade.togeduck.dto.request;
+
+public class SignUpDto {
+
+    private String userId;
+    private String password;
+    private String nickname;
+    private String email;
+}
