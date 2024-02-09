c361e21956ea532535793223c996ad2309d67778
swkim0911 <kimsungwon364@gmail.com>
2024-02-09 21:46:43 +0900

    Feat: SignUpDto 검증 로직 추가 아이디, 비밀번호, 닉네임 이메일 validation 애노테이션 추가

--- BE/src/main/java/com/lisade/togeduck/dto/request/SignUpDto.java
+++ BE/src/main/java/com/lisade/togeduck/dto/request/SignUpDto.java
@@ -1,9 +1,31 @@
 package com.lisade.togeduck.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpDto {
 
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대/소문자, 숫자, 특수문자를 사용해 주세요.")
     private String password;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 }
