441d3d2fc4922bb06d269fb7e9e1d95e624701f2
swkim0911 <kimsungwon364@gmail.com>
2024-02-10 16:29:27 +0900

    Test: UserRepositoryTest 클래스 existUserInfo 테스트 작성 DB에 저장된 사용자 정보가 조회되는지 확인

--- BE/src/test/java/com/lisade/togeduck/repository/UserRepositoryTest.java
+++ BE/src/test/java/com/lisade/togeduck/repository/UserRepositoryTest.java
@@ -1,9 +1,10 @@
 package com.lisade.togeduck.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.lisade.togeduck.dto.request.SignUpDto;
 import com.lisade.togeduck.entity.User;
 import com.lisade.togeduck.mapper.UserMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,7 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    @DisplayName("사용자가 DB에 잘 저장되는지 확인한다.")
+    @DisplayName("사용자가 DB에 잘 저장되는지 확인")
     void saveUser() {
         //given
         SignUpDto signUpDto = SignUpDto.builder()
@@ -30,6 +31,33 @@ class UserRepositoryTest {
         User savedUser = userRepository.save(user);
 
         //then
-        Assertions.assertThat(savedUser.getUserId()).isEqualTo(user.getUserId());
+        assertThat(savedUser.getUserId()).isEqualTo(user.getUserId());
+        assertThat(savedUser.getPassword()).isEqualTo(user.getPassword());
+        assertThat(savedUser.getNickname()).isEqualTo(user.getNickname());
+        assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
+    }
+
+    @Test
+    @DisplayName("DB에 저장된 사용자 정보가 제대로 조회되는지 확인")
+    void existId() {
+        //given
+        SignUpDto signUpDto = SignUpDto.builder()
+            .userId("testId")
+            .password("password12@")
+            .nickname("nickname")
+            .email("right@email.com")
+            .build();
+        User user = UserMapper.toUser(signUpDto);
+
+        //when
+        userRepository.save(user);
+        boolean existId = userRepository.existsByUserId(signUpDto.getUserId());
+        boolean existNickname = userRepository.existsByNickname(signUpDto.getNickname());
+        boolean existEmail = userRepository.existsByEmail(signUpDto.getEmail());
+
+        //then
+        assertThat(existId).isTrue();
+        assertThat(existNickname).isTrue();
+        assertThat(existEmail).isTrue();
     }
 }
