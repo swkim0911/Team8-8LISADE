d54fdf07071da31372cc6d909b487937d2187fc1
YUJEONG YUN <yunyj99@naver.com>
2024-02-11 18:36:51 +0900

    Refactor: 예약 현황 상세 페이지 수정#16
    - 기사 배차 전후에 따라 보이는 기사 정보 컨텐츠 수정
    - 작은 화면 사이즈에서는 아래 컨텐츠가 잘려서 스크롤뷰 추가

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -48,4 +49,5 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 }
--- AOS/app/src/main/java/com/softeer/togeduck/ui/reservation_status/ReservationStatusDetailActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/reservation_status/ReservationStatusDetailActivity.kt
@@ -2,11 +2,24 @@ package com.softeer.togeduck.ui.reservation_status
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.softeer.togeduck.R
+import com.softeer.togeduck.databinding.ActivityReservationStatusDetailBinding
 
 class ReservationStatusDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReservationStatusDetailBinding
+    private val reserveDetailViewModel: ReservationStatusDetailViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation_status_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reservation_status_detail)
+
+        init()
+    }
+
+    private fun init() {
+        binding.vm = reserveDetailViewModel
     }
 }
