f69875e6504c89b5e97728a6e903752ac9ac3241
ssangdon <tngustkdehs@naver.com>
2024-02-11 15:38:00 +0900

    Refactor: 로그인/회원가입 프래그먼트에 데이터바인딩 적용

--- AOS/app/src/main/java/com/softeer/togeduck/ui/intro/LoginFragment.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/intro/LoginFragment.kt
@@ -6,24 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.softeer.togeduck.R
+import com.softeer.togeduck.databinding.FragmentLoginBinding
+
 class LoginFragment : Fragment() {
 
+    private lateinit var binding : FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.register_btn).setOnClickListener {
+        binding.registerBtn.setOnClickListener{
             findNavController().navigate(R.id.action_intro_login_to_intro_register)
         }
-        view.findViewById<Button>(R.id.login_btn).setOnClickListener {
+        binding.loginBtn.setOnClickListener{
             findNavController().navigate(R.id.mainActivity)
         }
     }
