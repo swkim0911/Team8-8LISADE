821615f914e5f94d5160899ef6fa1e44f5e19db3
Hymin2 <khm8246@gmail.com>
2024-02-12 14:11:29 +0900

    Feat: UserRouteRepository 생성 #58
    - 구현 내용
        1. UserRouteRepository를 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/repository/UserRouteRepository.java
@@ -0,0 +1,8 @@
+package com.lisade.togeduck.repository;
+
+import com.lisade.togeduck.entity.UserRoute;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRouteRepository extends JpaRepository<UserRoute, Long> {
+
+}
