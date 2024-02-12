b226927468f84d62bdc82868ec72d69d72440241
Hymin2 <khm8246@gmail.com>
2024-02-12 15:07:40 +0900

    Feat: SeatController seat 등록 API 추가 #58
    - 구현 내용
        1. 좌석을 등록하는 API를 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/controller/SeatController.java
+++ BE/src/main/java/com/lisade/togeduck/controller/SeatController.java
@@ -1,10 +1,13 @@
 package com.lisade.togeduck.controller;
 
+import com.lisade.togeduck.dto.request.SeatRegistrationDto;
 import com.lisade.togeduck.dto.response.SeatListDto;
 import com.lisade.togeduck.service.SeatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,13 @@ public class SeatController {
         @PathVariable(name = "festival_id") Long festivalId,
         @PathVariable(name = "route_id") Long routeId) {
         return seatService.getList(routeId);
+    }
+
+    @PatchMapping("/seats")
+    public void register(
+        @PathVariable(name = "festival_id") Long festivalId,
+        @PathVariable(name = "route_id") Long routeId,
+        @RequestBody SeatRegistrationDto seatRegistration) {
+        seatService.register(routeId, seatRegistration);
     }
 }
