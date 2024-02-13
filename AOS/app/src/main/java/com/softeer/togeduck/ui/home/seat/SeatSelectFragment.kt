0c800ef90c1a6ab5decb7e465a6efd6649c6a256
YUJEONG YUN <yunyj99@naver.com>
2024-02-13 12:15:19 +0900

    Feat: 좌석 선택 네비게이션 화면 이동 구현 #67

---
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatSelectFragment.kt
@@ -0,0 +1,30 @@
+package com.softeer.togeduck.ui.home.seat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.softeer.togeduck.R
+
+class SeatSelectFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_seat_select, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
