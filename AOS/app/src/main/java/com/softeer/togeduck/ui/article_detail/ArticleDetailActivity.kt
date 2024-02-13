a3016af958829ee1f333cb9d59d793ff1278353a
ssangdon <tngustkdehs@naver.com>
2024-02-14 02:02:31 +0900

    Feat: 상세페이지 네비게이션 추가

---
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/article_detail/ArticleDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.softeer.togeduck.ui.article_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.softeer.togeduck.R
+
+class ArticleDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
