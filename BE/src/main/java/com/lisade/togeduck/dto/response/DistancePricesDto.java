40bc8e1e7c5fafd2d21164c6068dc928681ccb2b
Hymin2 <khm8246@gmail.com>
2024-02-14 11:18:32 +0900

    Feat: 계산된 거리와 요금표를 요청하는 응답에 대한 Dto 생성 #63
    - 구현 내용
        1. 출발지와 목적지의 거리와 해당 거리만큼의 요금표를 응답해주는 API에 대한 Dto를 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/dto/response/DistancePricesDto.java
@@ -0,0 +1,29 @@
+package com.lisade.togeduck.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DistancePricesDto {
+
+    private Integer distance;
+    private String expectedTime;
+    List<BusInfo> busInfos;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BusInfo {
+
+        private Long id;
+        private String busType;
+        private Integer price;
+    }
+}
