e8fc807f25e43b8d607ba3f99651cac430c8e01d
Hymin2 <khm8246@gmail.com>
2024-02-14 11:10:14 +0900

    Feat: TMap API 요청 결과가 저장될 Dto 생성 #63
    - 구현 내용
        1. TMap API으로 요청할 때 해당 결과가 저장될 Dto를 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/dto/response/TMapResultDto.java
@@ -0,0 +1,39 @@
+package com.lisade.togeduck.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TMapResultDto {
+
+    private String type;
+    private List<Features> features;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Features {
+
+        private String type;
+        private Properties properties;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Properties {
+
+        private Integer totalDistance;
+        private Integer totalTime;
+        private Integer totalFare;
+        private Integer taxiFare;
+    }
+}
