cdb362a8f4b77810f73a0ebd5fc545772c204e8e
Hymin2 <khm8246@gmail.com>
2024-02-14 11:13:21 +0900

    Feat: TMap API 요청 및 결과 저장 기능 추가 #63
    - 구현 내용
        1. TMap의 자동차 주행 거리와 예상 시간을 요청하고 응답을 저장하는 기능을 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/service/LocationService.java
+++ BE/src/main/java/com/lisade/togeduck/service/LocationService.java
@@ -1,22 +1,46 @@
 package com.lisade.togeduck.service;
 
 import com.lisade.togeduck.dto.response.LocationListDto;
+import com.lisade.togeduck.dto.response.TMapResultDto;
 import com.lisade.togeduck.entity.City;
 import com.lisade.togeduck.entity.Station;
 import com.lisade.togeduck.exception.StationNotFoundException;
 import com.lisade.togeduck.mapper.LocationMapper;
 import com.lisade.togeduck.repository.CityRepository;
 import com.lisade.togeduck.repository.StationRepository;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LocationService {
 
+    @Setter
+    @Value(("${T_MAP_URL}"))
+    private String T_MAP_URL;
+    @Setter
+    @Value("${T_MAP_APP_KEY}")
+    private String T_MAP_APP_KEY;
+
+    private final RestTemplate restTemplate;
     private final CityRepository cityRepository;
     private final StationRepository stationRepository;
+    private final FestivalService festivalService;
 
     public LocationListDto getList() {
         List<City> cities = getCities();
@@ -30,5 +54,36 @@ public class LocationService {
 
     public Station getStation(Long stationId) {
         return stationRepository.findById(stationId).orElseThrow(StationNotFoundException::new);
+    }
+
+    private TMapResultDto requestTMap(Double startX, Double startY, Double endX, Double endY) {
+        UriComponents uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(
+                T_MAP_URL)
+            .queryParam("version", 1)
+            .encode(StandardCharsets.UTF_8)
+            .build();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        httpHeaders.set("appKey", T_MAP_APP_KEY);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+
+        body.add("totalValue", 2);
+        body.add("startX", startX);
+        body.add("startY", startY);
+        body.add("endX", endX);
+        body.add("endY", endY);
+
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(body,
+            httpHeaders);
+
+        ResponseEntity<TMapResultDto> response = restTemplate.postForEntity(
+            uriComponentsBuilder.toUri(),
+            httpEntity,
+            TMapResultDto.class);
+
+        return response.getBody();
     }
 }
