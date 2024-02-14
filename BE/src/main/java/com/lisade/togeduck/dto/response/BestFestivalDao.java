68e71b6652ce176e0131ae03927f3846652ccfbf
dlwnsgus07 <dlwnsgus07@naver.com>
2024-02-14 22:06:31 +0900

    Feat : 인기 배너를 전달할 DTO 및 DAO 생성
    사용자들에게 인기배너를 전달하는 Dto 생성

---
+++ BE/src/main/java/com/lisade/togeduck/dto/response/BestFestivalDao.java
@@ -0,0 +1,14 @@
+package com.lisade.togeduck.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class BestFestivalDao {
+
+    Long id;
+    String path;
+}
