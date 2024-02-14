11f184ba064c5076c44cc40ec8afcc31c4a40269
dlwnsgus07 <dlwnsgus07@naver.com>
2024-02-14 22:09:26 +0900

    Feat : BestFestivalResponse 매퍼 추가
    BestFestivalDao 객체를 BestFestivalResponse로 매핑시키는 매퍼 추가

--- BE/src/main/java/com/lisade/togeduck/mapper/FestivalMapper.java
+++ BE/src/main/java/com/lisade/togeduck/mapper/FestivalMapper.java
@@ -1,5 +1,8 @@
 package com.lisade.togeduck.mapper;
 
+import com.lisade.togeduck.dto.response.BestFestivalDao;
+import com.lisade.togeduck.dto.response.BestFestivalResponse;
+import com.lisade.togeduck.dto.response.BestFestivalResponse.Banner;
 import com.lisade.togeduck.dto.response.FestivalDetailDto;
 import com.lisade.togeduck.dto.response.FestivalDto;
 import com.lisade.togeduck.entity.Festival;
@@ -7,6 +10,7 @@ import com.lisade.togeduck.entity.FestivalImage;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Slice;
 
@@ -21,6 +25,18 @@ public interface FestivalMapper {
     @Mapping(target = "paths", source = "festivalImages")
     @Mapping(target = "category", source = "category.type")
     FestivalDetailDto toFestivalDetailDto(Festival festival);
+
+    @Mapping(target = "totalSize", source = "totalSize")
+    @Mapping(target = "banner", source = "bestFestivalDao", qualifiedByName = "toBanner")
+    BestFestivalResponse toBestFestivalResponse(Integer totalSize,
+        List<BestFestivalDao> bestFestivalDao);
+
+    @Named("toBanner")
+    default List<Banner> daoToBanner(List<BestFestivalDao> bestFestivalDao) {
+        return bestFestivalDao.stream()
+            .map(dao -> Banner.builder().id(dao.getId()).path(dao.getPath()).build())
+            .toList();
+    }
 
     default List<String> festivalImagesToPaths(List<FestivalImage> festivalImages) {
         return festivalImages.stream()
