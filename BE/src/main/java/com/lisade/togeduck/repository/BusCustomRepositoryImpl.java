b590eb96beb7025090cb8ef7f60c4e4a7dcf09a1
Hymin2 <khm8246@gmail.com>
2024-02-15 00:24:18 +0900

    Feat: BusCustomRepositoryImpl 생성 및 가격표 반환 함수 구현 #63
    - 구현 내용
        1. BusCustomRepositoryImpl를 생성했습니다.
        2. distance에 맞는 버스들의 가격표를 반환하는 함수를 구현했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/repository/BusCustomRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.lisade.togeduck.repository;
+
+import static com.lisade.togeduck.entity.QBus.bus;
+import static com.lisade.togeduck.entity.QPriceTable.priceTable;
+
+import com.lisade.togeduck.dto.response.DistancePricesDto.BusInfo;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BusCustomRepositoryImpl implements BusCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<BusInfo> findBusInfoByDistance(Integer distance) {
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    BusInfo.class,
+                    bus.id,
+                    bus.type.stringValue(),
+                    priceTable.price))
+            .from(bus)
+            .innerJoin(priceTable)
+            .on(bus.eq(priceTable.bus)
+                .and(priceTable.distance.in(getPriceOfBusesByDistance(distance))))
+            .fetch();
+    }
+
+    private JPQLQuery<Integer> getPriceOfBusesByDistance(Integer distance) {
+        return JPAExpressions
+            .select(priceTable.distance.max())
+            .from(priceTable)
+            .where(priceTable.distance.lt(distance))
+            .groupBy(priceTable.bus.id);
+    }
+}
