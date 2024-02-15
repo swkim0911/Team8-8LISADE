e544c57719b6c8467143b719108b923a7ec86cca
Hymin2 <khm8246@gmail.com>
2024-02-15 11:35:29 +0900

    Refactor: SeatAlreadyRegisterException이 GeneralException을 상속받도록 변경 #85
    - 구현 내용
        1. SeatAlreadyRegisterException가 GeneralException을 상속받도록 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/exception/SeatAlreadyRegisterException.java
+++ BE/src/main/java/com/lisade/togeduck/exception/SeatAlreadyRegisterException.java
@@ -1,5 +1,11 @@
 package com.lisade.togeduck.exception;
 
-public class SeatAlreadyRegisterException extends RuntimeException {
+import com.lisade.togeduck.global.exception.GeneralException;
+import org.springframework.http.HttpStatus;
 
+public class SeatAlreadyRegisterException extends GeneralException {
+
+    public SeatAlreadyRegisterException(HttpStatus httpStatus, Object result) {
+        super(httpStatus, result);
+    }
 }
