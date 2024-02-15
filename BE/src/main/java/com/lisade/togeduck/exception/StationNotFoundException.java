2e74f30ce3fb59a699fc3ca50ebce753c49229c8
Hymin2 <khm8246@gmail.com>
2024-02-15 11:35:56 +0900

    Refactor: StationNotFoundException이 GeneralException을 상속받도록 변경 #85
    - 구현 내용
        1. StationNotFoundException가 GeneralException을 상속받도록 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/exception/StationNotFoundException.java
+++ BE/src/main/java/com/lisade/togeduck/exception/StationNotFoundException.java
@@ -1,5 +1,11 @@
 package com.lisade.togeduck.exception;
 
-public class StationNotFoundException extends RuntimeException {
+import com.lisade.togeduck.global.exception.GeneralException;
+import org.springframework.http.HttpStatus;
 
+public class StationNotFoundException extends GeneralException {
+
+    public StationNotFoundException(HttpStatus httpStatus, Object result) {
+        super(httpStatus, result);
+    }
 }
