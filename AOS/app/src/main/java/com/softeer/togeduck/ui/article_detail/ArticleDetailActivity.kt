74942dfccfba0ead46724bb2896203af1ffe1630
ssangdon <tngustkdehs@naver.com>
2024-02-15 11:26:24 +0900

    Feat: 상세정보/노선 네비게이션 연동완료#74

--- AOS/app/src/main/java/com/softeer/togeduck/ui/article_detail/ArticleDetailActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/article_detail/ArticleDetailActivity.kt
@@ -3,8 +3,10 @@ package com.softeer.togeduck.ui.article_detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.fragment.findNavController
 import com.softeer.togeduck.R
 import com.softeer.togeduck.databinding.ActivityArticleDetailBinding
 
@@ -14,14 +16,22 @@ class ArticleDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_article_detail)
+        setUpNavigator()
     }
 
     private fun setUpNavigator(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.detailFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
         binding.routeButton.setOnClickListener{
-
+            navController.navigate(R.id.action_articleDetailFragment_to_routeFragment)
+            binding.routeButton.isEnabled = false
+            binding.articleDetailButton.isEnabled = true
         }
-        binding.articleDetailButton.setOnClickListener{
 
+        binding.articleDetailButton.setOnClickListener{
+            navController.navigate(R.id.action_routeFragment_to_articleDetailFragment)
+            binding.routeButton.isEnabled = true
+            binding.articleDetailButton.isEnabled = false
         }
     }
 }
