7b3054e9b398c945310f3de228995533a99dc519
Hymin2 <khm8246@gmail.com>
2024-02-15 11:45:27 +0900

    Refactor: GeneralException의 필드 수정 #85
    - 구현 내용
        1. GeneralException의 필드를 Error 객체만 갖고 있도록 수정했습니다.

--- BE/src/main/java/com/lisade/togeduck/global/exception/GeneralException.java
+++ BE/src/main/java/com/lisade/togeduck/global/exception/GeneralException.java
@@ -2,13 +2,10 @@ package com.lisade.togeduck.global.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public class GeneralException extends RuntimeException {
 
-    private final HttpStatus httpStatus;
-
-    private final Object result;
+    private final Error error;
 }
