126584fcea29deade22e0058feca511a7c7df753
Hymin2 <khm8246@gmail.com>
2024-02-15 11:34:38 +0900

    Refactor: BusNotFoundException이 GeneralException을 상속받도록 변경 #63
    - 구현 내용
        1. BusNotFoundException을 GeneralException을 상속받도록 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/exception/BusNotFoundException.java
+++ BE/src/main/java/com/lisade/togeduck/exception/BusNotFoundException.java
@@ -1,5 +1,11 @@
 package com.lisade.togeduck.exception;
 
-public class BusNotFoundException extends RuntimeException {
+import com.lisade.togeduck.global.exception.GeneralException;
+import org.springframework.http.HttpStatus;
 
+public class BusNotFoundException extends GeneralException {
+
+    public BusNotFoundException(HttpStatus httpStatus, Object result) {
+        super(httpStatus, result);
+    }
 }
