89c5f5ae5ca14981b32b5dcc4e0ddc7b4bcb0873
Hymin2 <khm8246@gmail.com>
2024-02-15 14:01:33 +0900

    Feat: UserController에서 예외 처리 변경 #85
    - 구현 내용
        1. Error 객체가 파라미터로 있는 경우 해당 파라미터를 삭제했습니다.
        2. Error 객체로 예외 처리를 하는 경우를 삭제했습니다.
        3. CustomValidator를 삭제했습니다.

--- BE/src/main/java/com/lisade/togeduck/controller/UserController.java
+++ BE/src/main/java/com/lisade/togeduck/controller/UserController.java
@@ -1,31 +1,21 @@
 package com.lisade.togeduck.controller;
 
 import static com.lisade.togeduck.constant.SessionConst.LOGIN_USER;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
 
 import com.lisade.togeduck.annotation.Login;
 import com.lisade.togeduck.dto.request.LoginDto;
 import com.lisade.togeduck.dto.request.SignUpDto;
-import com.lisade.togeduck.dto.response.LoginEmptyFieldDto;
-import com.lisade.togeduck.dto.response.SignUpFailureDto;
 import com.lisade.togeduck.entity.User;
-import com.lisade.togeduck.exception.InvalidSignUpException;
-import com.lisade.togeduck.exception.LoginEmptyFieldException;
 import com.lisade.togeduck.exception.UnAuthenticationException;
 import com.lisade.togeduck.global.response.ApiResponse;
 import com.lisade.togeduck.service.UserService;
-import com.lisade.togeduck.validator.SignUpValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,12 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    private final SignUpValidator signUpValidator;
-
-    @InitBinder // UserController 요청에 대한 커스텀 validator 추가
-    public void validatorBinder(WebDataBinder binder) {
-        binder.addValidators(signUpValidator);
-    }
 
     @GetMapping("/{user_id}")
     public ResponseEntity<Object> checkUserId(@PathVariable(name = "user_id") String userId) {
@@ -51,25 +35,14 @@ public class UserController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Object> signUp(@RequestBody @Valid SignUpDto signUpDto, Errors errors) {
-
-        if (errors.hasErrors()) {
-            SignUpFailureDto signUpFailureDto = userService.validateSignUp(errors);
-            throw new InvalidSignUpException(BAD_REQUEST, signUpFailureDto);
-        }
+    public ResponseEntity<Object> signUp(@RequestBody @Valid SignUpDto signUpDto) {
         Long id = userService.join(signUpDto);
         return new ResponseEntity<>(ApiResponse.of(CREATED.value(), CREATED.name(), id), CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody @Valid LoginDto loginDto, Errors errors,
+    public ResponseEntity<Object> login(@RequestBody @Valid LoginDto loginDto,
         HttpServletRequest request) {
-
-        if (errors.hasErrors()) {
-            LoginEmptyFieldDto loginEmptyFieldDto = userService.validateLogin(errors);
-            throw new LoginEmptyFieldException(BAD_REQUEST, loginEmptyFieldDto);
-        }
-
         User findUser = userService.login(loginDto);
         HttpSession session = request.getSession();
         session.setAttribute(LOGIN_USER.getSessionName(), findUser);
@@ -77,10 +50,9 @@ public class UserController {
     }
 
     @GetMapping("/routes")
-    public ResponseEntity<Object> getRoutes(
-        @Login User user) {
+    public ResponseEntity<Object> getRoutes(@Login User user) {
         if (user == null) {
-            throw new UnAuthenticationException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+            throw new UnAuthenticationException();
         }
         return null;
     }
