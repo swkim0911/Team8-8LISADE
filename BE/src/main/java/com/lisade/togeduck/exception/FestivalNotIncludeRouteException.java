b748505e964e229f6bad729e8197a61152424dcb
Hymin2 <khm8246@gmail.com>
2024-02-15 14:19:01 +0900

    Refactor: FestivalNotIncludeRouteException 수정 #85
    - 구현 내용
        1. GeneralException을 상속받도록 수정했습니다.
        2. 기본 생성자를 생성했습니다.
        3. Error를 인스턴스로 받는 생성자를 생성했습니다.

--- BE/src/main/java/com/lisade/togeduck/exception/FestivalNotIncludeRouteException.java
+++ BE/src/main/java/com/lisade/togeduck/exception/FestivalNotIncludeRouteException.java
@@ -1,5 +1,15 @@
 package com.lisade.togeduck.exception;
 
-public class FestivalNotIncludeRouteException extends RuntimeException {
+import com.lisade.togeduck.global.exception.Error;
+import com.lisade.togeduck.global.exception.GeneralException;
 
+public class FestivalNotIncludeRouteException extends GeneralException {
+
+    public FestivalNotIncludeRouteException() {
+        super(Error.FESTIVAL_NOT_INCLUDE_ROUTE_ERROR);
+    }
+
+    public FestivalNotIncludeRouteException(Error error) {
+        super(error);
+    }
 }
