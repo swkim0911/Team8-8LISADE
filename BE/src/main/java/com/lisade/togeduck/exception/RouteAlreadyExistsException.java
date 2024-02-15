576e3511ae8d99c9a27e642c9326daf6d150c52a
Hymin2 <khm8246@gmail.com>
2024-02-15 14:19:18 +0900

    Refactor: RouteAlreadyExistsException 수정 #85
    - 구현 내용
        1. GeneralException을 상속받도록 수정했습니다.
        2. 기본 생성자를 생성했습니다.
        3. Error를 인스턴스로 받는 생성자를 생성했습니다.

--- BE/src/main/java/com/lisade/togeduck/exception/RouteAlreadyExistsException.java
+++ BE/src/main/java/com/lisade/togeduck/exception/RouteAlreadyExistsException.java
@@ -1,11 +1,15 @@
 package com.lisade.togeduck.exception;
 
+import com.lisade.togeduck.global.exception.Error;
 import com.lisade.togeduck.global.exception.GeneralException;
-import org.springframework.http.HttpStatus;
 
 public class RouteAlreadyExistsException extends GeneralException {
 
-    public RouteAlreadyExistsException(HttpStatus httpStatus, Object result) {
-        super(httpStatus, result);
+    public RouteAlreadyExistsException() {
+        super(Error.ROUTE_ALREADY_EXISTS_ERROR);
+    }
+
+    public RouteAlreadyExistsException(Error error) {
+        super(error);
     }
 }
