844a3b21ab0cb6b3975f555198620e83080873ca
YUJEONG YUN <yunyj99@naver.com>
2024-02-15 16:08:01 +0900

    Feat: 결제 화면에서 결제 완료 화면 이동 구현 #87

--- AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatPaymentFragment.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatPaymentFragment.kt
@@ -8,8 +8,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.softeer.togeduck.R
+import com.softeer.togeduck.databinding.FragmentSeatPaymentBinding
 
 class SeatPaymentFragment : Fragment() {
+    private var _binding: FragmentSeatPaymentBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,11 +22,19 @@ class SeatPaymentFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_seat_payment, container, false)
+        _binding = FragmentSeatPaymentBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.paymentBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_seatPaymentFragment_to_seatPaymentCompleteFragment)
+        }
     }
 }
