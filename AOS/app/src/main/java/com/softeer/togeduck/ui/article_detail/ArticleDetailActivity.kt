bb769f5c490109a553d391fe736075be36941fbc
ssangdon <tngustkdehs@naver.com>
2024-02-15 17:31:12 +0900

    Refactor: 스코프 함수 이용하여 코드정렬

--- AOS/app/src/main/java/com/softeer/togeduck/ui/article_detail/ArticleDetailActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/article_detail/ArticleDetailActivity.kt
@@ -31,23 +31,26 @@ class ArticleDetailActivity : AppCompatActivity() {
 
         binding.routeButton.setOnClickListener {
             navController.navigate(R.id.action_articleDetailFragment_to_routeFragment)
-            binding.routeButton.setTextColor(checkedColor)
-            binding.articleDetailButton.setTextColor(defaultColor)
-            binding.articleDetailIndicator.setBackgroundColor(transparentColor)
-            binding.routeIndicator.setBackgroundColor(checkedColor)
-            binding.routeButton.isEnabled = false
-            binding.articleDetailButton.isEnabled = true
-
+            binding.run{
+                routeButton.setTextColor(checkedColor)
+                articleDetailButton.setTextColor(defaultColor)
+                articleDetailIndicator.setBackgroundColor(transparentColor)
+                routeIndicator.setBackgroundColor(checkedColor)
+                routeButton.isEnabled = false
+                articleDetailButton.isEnabled = true
+            }
         }
 
         binding.articleDetailButton.setOnClickListener {
             navController.navigate(R.id.action_routeFragment_to_articleDetailFragment)
-            binding.routeButton.setTextColor(defaultColor)
-            binding.articleDetailButton.setTextColor(checkedColor)
-            binding.articleDetailIndicator.setBackgroundColor(checkedColor)
-            binding.routeIndicator.setBackgroundColor(transparentColor)
-            binding.routeButton.isEnabled = true
-            binding.articleDetailButton.isEnabled = false
+            binding.run{
+                routeButton.setTextColor(defaultColor)
+                articleDetailButton.setTextColor(checkedColor)
+                articleDetailIndicator.setBackgroundColor(checkedColor)
+                routeIndicator.setBackgroundColor(transparentColor)
+                routeButton.isEnabled = true
+                articleDetailButton.isEnabled = false
+            }
         }
     }
 }
