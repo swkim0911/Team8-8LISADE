71d8a5abe1d40f9a04300de2d00abb61dbba8e83
Hymin2 <khm8246@gmail.com>
2024-02-18 21:23:35 +0900

    Feat: RedisTemplate에 직렬화 추가 #106
    - 구현 내용
        1. RedisTemplate의 key와 value에서 String 직렬화 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/config/RedisConfig.java
+++ BE/src/main/java/com/lisade/togeduck/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -19,6 +20,9 @@ public class RedisConfig {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        
         return redisTemplate;
     }
 }
