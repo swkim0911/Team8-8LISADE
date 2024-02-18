b24eb5df6194487c782bc8b4599f289006bc71f5
swkim0911 <kimsungwon364@gmail.com>
2024-02-18 15:16:38 +0900

    Feat: FestivalRoutesDto 클래스 생성 페스티벌 경로 리스트 반환을 위한 dto 생성

---
+++ BE/src/main/java/com/lisade/togeduck/dto/response/FestivalRoutesDto.java
@@ -0,0 +1,22 @@
+package com.lisade.togeduck.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FestivalRoutesDto {
+
+    private Long id;
+    private LocalDateTime startedAt;
+    private String station;
+    private Integer price;
+    private String status;
+    private Integer totalSeats;
+    private Integer reservedSeats;
+}
