21ef3ff884b63dea385d99306ee8a9cabf139993
swkim0911 <kimsungwon364@gmail.com>
2024-02-18 20:37:05 +0900

    Feat: FestivalNotFoundException 생성 db에서 festival id를 찾을 수 없는 경우 발생

--- BE/src/main/java/com/lisade/togeduck/global/exception/Error.java
+++ BE/src/main/java/com/lisade/togeduck/global/exception/Error.java
@@ -14,6 +14,7 @@ public enum Error {
     SEAT_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "Seat를 찾을 수 없습니다."),
     ROUTE_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "Route를 찾을 수 없습니다."),
     STATION_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "Station을 찾을 수 없습니다."),
+    FESTIVAL_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "Festival을 찾을 수 없습니다."),
     USER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "아이디 또는 비밀번호가 잘못되었습니다."),
     FESTIVAL_NOT_INCLUDE_ROUTE_ERROR(HttpStatus.NOT_FOUND, "해당 Festival에 Route를 찾을 수 없습니다."),
 
