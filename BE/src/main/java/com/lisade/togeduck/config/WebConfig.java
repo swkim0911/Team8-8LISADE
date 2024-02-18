1a189f0fb3e839e9f42769159bf8557977cf4171
dlwnsgus07 <dlwnsgus07@naver.com>
2024-02-18 19:58:54 +0900

    Fix : 기존에 HttpMessageConverter가 String을 우선적으로 컨트롤 하면서 발생하는 Cast 에러 해결
    기존에 String이 HttpMessageConverter의 2순위로서 발생했던 에러를 수정하기 위해 Jackson HttpMessageHandler가 String 보다 우선적으로 작동하도록 변경

--- BE/src/main/java/com/lisade/togeduck/config/WebConfig.java
+++ BE/src/main/java/com/lisade/togeduck/config/WebConfig.java
@@ -5,6 +5,8 @@ import com.lisade.togeduck.util.FestivalStatusRequestConverter;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginArgumentResolver());
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.removeIf(
+            httpMessageConverter -> httpMessageConverter instanceof MappingJackson2HttpMessageConverter);
+        converters.add(1, new MappingJackson2HttpMessageConverter());
     }
 }
