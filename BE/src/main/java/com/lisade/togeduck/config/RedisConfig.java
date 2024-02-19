bdadac2f101934dcb4bde61bbf416d85d113c9af
Hymin2 <khm8246@gmail.com>
2024-02-20 01:55:21 +0900

    Chore: RedisConfig 설정 추가 #126
    - 구현 내용
        1. RedisTemplate의 ValueSerializer에 LocationCacheValue 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/config/RedisConfig.java
+++ BE/src/main/java/com/lisade/togeduck/config/RedisConfig.java
@@ -1,11 +1,13 @@
 package com.lisade.togeduck.config;
 
+import com.lisade.togeduck.cache.LocationCacheValue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -28,6 +30,9 @@ public class RedisConfig {
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setValueSerializer(
+            new Jackson2JsonRedisSerializer<>(LocationCacheValue.class));
 
         return redisTemplate;
     }
