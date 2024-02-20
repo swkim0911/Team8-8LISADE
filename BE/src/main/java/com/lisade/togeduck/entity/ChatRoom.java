27b91a92a6449805c1ee16725c4320be9464d501
swkim0911 <kimsungwon364@gmail.com>
2024-02-21 02:12:05 +0900

    Feat: ChatRoom 엔티티 생성 채팅방 엔티티 생성. 채팅방과 user의 관계는 n:1

---
+++ BE/src/main/java/com/lisade/togeduck/entity/ChatRoom.java
@@ -0,0 +1,35 @@
+package com.lisade.togeduck.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
--- BE/src/main/java/com/lisade/togeduck/entity/User.java
+++ BE/src/main/java/com/lisade/togeduck/entity/User.java
@@ -1,6 +1,7 @@
 package com.lisade.togeduck.entity;
 
 import com.lisade.togeduck.entity.enums.AuthorityType;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,7 +9,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,6 +43,9 @@ public class User extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     @Column(name = "authority", nullable = false)
     private AuthorityType authority;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<ChatRoom> chatRooms;
 
     @Builder
     public User(Long id, String userId, String password, String nickname, String email,
