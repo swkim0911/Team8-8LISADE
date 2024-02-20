a690b7f2cb08d53632025e90f51f4f9d93f923ec
ssangdon <tngustkdehs@naver.com>
2024-02-20 18:19:39 +0900

    FIx: 지역선택 다이얼로그 선택시 노선개설 페이지내 시작경로 적용#140

--- AOS/app/src/main/java/com/softeer/togeduck/ui/home/open_route/OpenRouteFragment.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/home/open_route/OpenRouteFragment.kt
@@ -1,10 +1,12 @@
 package com.softeer.togeduck.ui.home.open_route
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.softeer.togeduck.R
 import com.softeer.togeduck.databinding.FragmentOpenRouteBinding
@@ -13,6 +15,7 @@ import com.softeer.togeduck.databinding.FragmentOpenRouteBinding
 class OpenRouteFragment : Fragment() {
     private var _binding: FragmentOpenRouteBinding? = null
     private val binding get() = _binding!!
+    private val regionListViewModel: RegionListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +28,13 @@ class OpenRouteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init()
+
     }
 
     private fun init(){
+        binding.vm = regionListViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        Log.d("TESTLOG2",regionListViewModel.selectedRegion.value!!)
         binding.leftLayoutView.setOnClickListener {
             RegionListDialog().show(parentFragmentManager, "ListDialogFragment")
         }
