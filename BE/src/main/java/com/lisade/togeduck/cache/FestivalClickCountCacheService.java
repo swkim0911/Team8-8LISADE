af5a624d99d3066b75ec93c84ed24c525177b351
Hymin2 <khm8246@gmail.com>
2024-02-21 01:53:52 +0900

    Feat: FestivalClickCountCacheService 생성 및 조회수 저장 기능 구현 #127
    - 구현 내용
        1. Festival의 조회수와 관련된 Service인 FestivalClickCountCacheService를 생성했습니다.
        2. 조회수를 증가시키는 함수를 구현했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/cache/FestivalClickCountCacheService.java
@@ -0,0 +1,41 @@
+package com.lisade.togeduck.cache;
+
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FestivalClickCountCacheService {
+
+    private final UserClickLogCacheService userClickLogCacheService;
+    private final FestivalClickCountCacheRepository festivalClickCountCacheRepository;
+
+    public void increase(String userId, Long festivalId) {
+        if (userClickLogCacheService.isClick(userId, festivalId)) {
+            return;
+        }
+
+        userClickLogCacheService.addFestival(userId, festivalId);
+        
+        Optional<FestivalClickCountCacheValue> festivalClickCountCacheValue =
+            festivalClickCountCacheRepository.findByFestivalId(festivalId);
+
+        if (festivalClickCountCacheValue.isEmpty()) {
+            save(FestivalClickCountCacheValue.of(festivalId, 1L));
+
+            return;
+        }
+
+        festivalClickCountCacheValue.get().increaseClickCount();
+        save(festivalClickCountCacheValue.get());
+    }
+
+    public void save(FestivalClickCountCacheValue festivalClickCountCacheValue) {
+        festivalClickCountCacheRepository.save(festivalClickCountCacheValue);
+    }
+
+    public void deleteAll() {
+        festivalClickCountCacheRepository.deleteAll();
+    }
+}
