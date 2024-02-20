24e240d45d386e903f2cfd4ede575a4065e534cd
swkim0911 <kimsungwon364@gmail.com>
2024-02-20 14:41:47 +0900

    Feat: WebSocket Config 클래스 구현 stomp 프로토콜 사용을 위한 config

---
+++ BE/src/main/java/com/lisade/togeduck/config/StompWebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.lisade.togeduck.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@Configuration
+@RequiredArgsConstructor
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat"); // 웹소켓 연결을 위해 3-handshake 수행하기 위한 URL
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/queue", "/topic"); // 스프링 내장 브로커 사용. client에게 메시지 보낼때
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors();
+    }
+}
