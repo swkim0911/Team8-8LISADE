c0d86ea4bf042e44ddce63cdb6b1f2c3074ff91c
Hymin2 <khm8246@gmail.com>
2024-02-22 01:35:08 +0900

    Feat: BatchConfig에서 ItemReader 추가 #165
    - 구현 내용
        1. Batch에서 데이터를 읽어올 ItemReader를 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/config/BatchConfig.java
+++ BE/src/main/java/com/lisade/togeduck/config/BatchConfig.java
@@ -1,5 +1,7 @@
 package com.lisade.togeduck.config;
 
+import com.lisade.togeduck.dto.BatchDto;
+import java.time.LocalDate;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -8,6 +10,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -34,9 +39,36 @@ public class BatchConfig {
     @Bean
     public Step step(JobRepository jobRepository) {
         // TODO ItemReader, ItemProcessor, ItemWriter 구현하기
-        
+
         return new StepBuilder("step", jobRepository)
-            .chunk(1000, transactionManager())
+            .<BatchDto, BatchDto>chunk(1000, transactionManager())
+            .reader(itemReader())
+            .writer(itemWriter())
             .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<BatchDto> itemReader() {
+        return new JdbcCursorItemReaderBuilder<BatchDto>()
+            .name("itemReader")
+            .sql(
+                "select f.id as id, f.title as title, sum(fv.count) as count "
+                    + "from festival as f "
+                    + "inner join festival_view as fv "
+                    + "on f.id = fv.festival_id "
+                    + "where fv.measurement_at > ?"
+                    + "group by f.id;")
+            .beanRowMapper(BatchDto.class)
+            .queryArguments(LocalDate.now().minusWeeks(1L))
+            .fetchSize(1000)
+            .dataSource(dataSource)
+            .build();
+    }
+
+    @Bean
+    public ItemWriter<BatchDto> itemWriter() {
+        return items -> {
+            System.out.println(items.size());
+        };
     }
 }
