3aec33d52a27c4a37c00f3c46e4c3aa555302ac7
Hymin2 <khm8246@gmail.com>
2024-02-21 11:55:45 +0900

    Chore: BatchConfig 파일 생성 #161
    - 구현 내용
        1. SpringBatch의 Bean 등록을 위한 BatchConfig을 생성했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/config/BatchConfig.java
@@ -0,0 +1,12 @@
+package com.lisade.togeduck.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class BatchConfig {
+
+}
