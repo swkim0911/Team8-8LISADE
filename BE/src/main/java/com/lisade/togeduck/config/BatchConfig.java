c37f9f75abe3c4ef24f63e964a033fe3d2584686
Hymin2 <khm8246@gmail.com>
2024-02-21 17:17:23 +0900

    Feat: Batch 작업인 calcPopularScoreJob Bean 등록 #165
    - 구현 내용
        1. BatchConfig에서 calcPopularScoreJob을 Bean으로 등록했습니다.

--- BE/src/main/java/com/lisade/togeduck/config/BatchConfig.java
+++ BE/src/main/java/com/lisade/togeduck/config/BatchConfig.java
@@ -1,12 +1,42 @@
 package com.lisade.togeduck.config;
 
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
 @RequiredArgsConstructor
 public class BatchConfig {
 
+    private final DataSource dataSource;
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public Job calcPopularScoreJob(JobRepository jobRepository) {
+        return new JobBuilder("calcPopularScoreJob", jobRepository)
+            .start(step(jobRepository))
+            .build();
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository) {
+        // TODO ItemReader, ItemProcessor, ItemWriter 구현하기
+        
+        return new StepBuilder("step", jobRepository)
+            .chunk(1000, transactionManager())
+            .build();
+    }
 }
