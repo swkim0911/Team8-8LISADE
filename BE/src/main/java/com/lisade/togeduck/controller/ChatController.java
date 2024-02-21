951c9ab869f427dbc91b07fba2953642c73feb1a
swkim0911 <kimsungwon364@gmail.com>
2024-02-21 13:44:22 +0900

    Feat: ChatController 클래스 createChatRoom 생성 controller layer 에서 채팅방 생성

--- BE/src/main/java/com/lisade/togeduck/controller/ChatController.java
+++ BE/src/main/java/com/lisade/togeduck/controller/ChatController.java
@@ -1,10 +1,13 @@
 package com.lisade.togeduck.controller;
 
+import com.lisade.togeduck.annotation.Login;
 import com.lisade.togeduck.dto.chat.ChatMessage;
+import com.lisade.togeduck.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
@@ -12,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ChatController {
 
     private final SimpMessagingTemplate template;
+    private final ChatService chatService;
+
+    @PostMapping("chatrooms")
+    public Long createChatRoom(@Login User user) {
+        return chatService.createChatRoom(user.getId());
+    }
 
     // /pub/chat/enter
     @MessageMapping(value = "/chat/enter")
--- BE/src/main/java/com/lisade/togeduck/repository/ChatRoomRepository.java
+++ BE/src/main/java/com/lisade/togeduck/repository/ChatRoomRepository.java
@@ -1,9 +1,12 @@
 package com.lisade.togeduck.repository;
 
+import com.lisade.togeduck.entity.ChatRoom;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomRepository, Long> {
-    
+
+    Optional<ChatRoom> findByChatRoomId(Long chatRoomId);
 }
