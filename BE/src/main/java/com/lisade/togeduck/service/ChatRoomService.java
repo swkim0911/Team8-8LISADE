3cc159ee6b05496e202243b93b53666ff61da7bf
swkim0911 <kimsungwon364@gmail.com>
2024-02-22 23:56:36 +0900

    Fix: ChatRoomService 변경 ChatRoom 필드가 변경됨에 따라 ChatRoomSerivce 도 변경

--- BE/src/main/java/com/lisade/togeduck/service/ChatRoomService.java
+++ BE/src/main/java/com/lisade/togeduck/service/ChatRoomService.java
@@ -3,12 +3,13 @@ package com.lisade.togeduck.service;
 import com.lisade.togeduck.dto.response.ChatRoomListResponse;
 import com.lisade.togeduck.entity.ChatRoom;
 import com.lisade.togeduck.entity.Festival;
+import com.lisade.togeduck.entity.Route;
 import com.lisade.togeduck.entity.User;
 import com.lisade.togeduck.entity.UserChatRoom;
 import com.lisade.togeduck.exception.ChatRoomNotFoundException;
-import com.lisade.togeduck.exception.FestivalNotFoundException;
+import com.lisade.togeduck.exception.RouteNotFoundException;
 import com.lisade.togeduck.repository.ChatRoomRepository;
-import com.lisade.togeduck.repository.FestivalRepository;
+import com.lisade.togeduck.repository.RouteRepository;
 import com.lisade.togeduck.repository.UserChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -22,14 +23,15 @@ public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final UserChatRoomRepository userChatRoomRepository;
-    private final FestivalRepository festivalRepository;
+    private final RouteRepository routeRepository;
 
     @Transactional
-    public void create(User user, Long festivalId) {
-        Festival festival = festivalRepository.findById(festivalId)
-            .orElseThrow(FestivalNotFoundException::new);
-
+    public void create(User user, Long routeId) {
+        Route findRoute = routeRepository.findById(routeId)
+            .orElseThrow(RouteNotFoundException::new);
+        Festival festival = findRoute.getFestival();
         ChatRoom chatRoom = ChatRoom.builder()
+            .route(findRoute)
             .roomName(festival.getTitle() + " 채팅방")
             .numberOfMembers(0)
             .thumbnailPath(festival.getThumbnailPath())
