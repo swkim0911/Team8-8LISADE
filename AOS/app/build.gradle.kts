e4495bdb399e0fb03597c2bf1d18277501e9d110
Hymin2 <khm8246@gmail.com>
2024-02-23 11:51:40 +0900

    Chore: Stomp 의존성 추가 #186
    - 구현 내용
        1. Stomp의 의존성을 추가했습니다.

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
 
+    implementation("com.github.NaikSoftware:StompProtocolAndroid:1.6.6")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.5")
+    implementation("io.reactivex.rxjava2:rxandroid:2.1.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
