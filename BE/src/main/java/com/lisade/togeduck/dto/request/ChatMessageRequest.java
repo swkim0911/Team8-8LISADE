43fdb05c79ea001054b950476ee20b64cdfe2ed8
Hymin2 <khm8246@gmail.com>
2024-02-24 04:22:33 +0900

    Feat: ChatMessageRequest 수정 #198
    - 구현 내용
        1. User와 관련된 필드를 제거했습니다.

--- BE/src/main/java/com/lisade/togeduck/dto/request/ChatMessageRequest.java
+++ BE/src/main/java/com/lisade/togeduck/dto/request/ChatMessageRequest.java
@@ -1,12 +1,9 @@
 package com.lisade.togeduck.dto.request;
 
-import com.lisade.togeduck.entity.enums.MessageAction;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Builder
@@ -14,12 +11,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ChatMessageRequest {
 
-    private Long userId;
     private Long roomId;
-    private String sender;
-    @Setter
     private String message;
-    private MessageAction action;
-    private LocalDateTime createdAt;
-
+    private String action;
+    private String createdAt;
 }
