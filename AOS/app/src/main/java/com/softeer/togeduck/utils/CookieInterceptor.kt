1fcdbdeaad5567b4446d3b29485ecf6ba2632068
YUJEONG YUN <yunyj99@naver.com>
2024-02-24 02:17:55 +0900

    Fix: 헤더에 세션ID값 제대로 못보내는 오류 수정

--- AOS/app/src/main/java/com/softeer/togeduck/utils/CookieInterceptor.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/utils/CookieInterceptor.kt
@@ -1,6 +1,8 @@
 package com.softeer.togeduck.utils
 
 import com.softeer.togeduck.data.local.datasource.UserDataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -9,11 +11,23 @@ class CookieInterceptor @Inject constructor(private val userDataStore: UserDataS
     Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val requestBuilder = chain.request().newBuilder()
-        val cookie = userDataStore.getUserSessionId.toString()
+        val request = chain.request()
 
-        requestBuilder.addHeader("Cookie", cookie)
+        if (isExcludedApi(request.url.toString())) {
+            return chain.proceed(request)
+        }
+
+        val requestBuilder = request.newBuilder()
+
+        val cookie = runBlocking {
+            userDataStore.getUserSessionId.first().toString()
+        }
+        requestBuilder.addHeader("Cookie", "JSESSIONID=${cookie};")
 
         return chain.proceed(requestBuilder.build())
+    }
+
+    private fun isExcludedApi(url: String): Boolean {
+        return url.contains("/users/login")
     }
 }
