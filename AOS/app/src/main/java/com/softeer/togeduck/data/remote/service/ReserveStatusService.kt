09105d06c3cfda518c966caa813a3e395bbd65fc
YUJEONG YUN <yunyj99@naver.com>
2024-02-23 11:32:09 +0900

    Feat: 예약 현황 Service에 api 등록 #110

--- AOS/app/src/main/java/com/softeer/togeduck/data/remote/service/ReserveStatusService.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/remote/service/ReserveStatusService.kt
@@ -1,4 +1,22 @@
 package com.softeer.togeduck.data.remote.service
 
+import com.softeer.togeduck.data.dto.response.reserve_status.ReserveStatusDetailResponse
+import com.softeer.togeduck.data.dto.response.reserve_status.ReserveStatusResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ReserveStatusService {
+    @GET("users/routes")
+    suspend fun getReserveStatusList(
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<ReserveStatusResponse>
+
+
+    @GET("users/routes/{route_id}")
+    suspend fun getReserveStatusDetail(
+        @Path("route_id") routeId: Int
+    ): Response<ReserveStatusDetailResponse>
 }
