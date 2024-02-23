7dadc7c561fe0b5f25d850ca5f6b213922941890
YUJEONG YUN <yunyj99@naver.com>
2024-02-23 12:15:14 +0900

    Feat: 숫자 세 자리 단위마다 , 붙이는 확장 함수 추가  #110

--- AOS/app/src/main/java/com/softeer/togeduck/utils/Extensions.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/utils/Extensions.kt
@@ -1,7 +1,14 @@
 package com.softeer.togeduck.utils
 
 import android.content.res.Resources
+import java.text.DecimalFormat
 
 
 fun Int.fromDpToPx(): Int =
     (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun Int.addCommas(): String {
+    val dec = DecimalFormat("#,###")
+
+    return dec.format(this)
+}
