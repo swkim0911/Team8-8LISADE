42eb49541313e006982e1ca0f3a069507db36496
YUJEONG YUN <yunyj99@naver.com>
2024-02-24 05:05:22 +0900

    Feat: 예약 상세 내역 모집 현황에 따라 태그 구현 #110

--- AOS/app/src/main/java/com/softeer/togeduck/utils/BindingAdapter.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/utils/BindingAdapter.kt
@@ -1,13 +1,41 @@
 package com.softeer.togeduck.utils
 
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-
+import com.softeer.togeduck.R
+import com.softeer.togeduck.data.dto.response.reserve_status.reserve_detail.ReserveStatus
 
 @BindingAdapter("imgLoad")
 fun loadImg(view: ImageView, url: String) {
     Glide.with(view.context)
         .load(url)
         .into(view)
+}
+
+@BindingAdapter("recruitmentStatusTag")
+fun loadRecruitmentStatusTag(view: TextView, status: ReserveStatus) {
+    view.text = status.value
+
+    view.setBackgroundColor(
+        when (status) {
+            ReserveStatus.RECRUIT -> {
+                ContextCompat.getColor(view.context, R.color.mint)
+            }
+
+            ReserveStatus.RECRUIT_COMPLETE -> {
+                ContextCompat.getColor(view.context, R.color.navy300)
+            }
+
+            ReserveStatus.OPERATION_CONFIRM -> {
+                ContextCompat.getColor(view.context, R.color.navy800)
+            }
+
+            ReserveStatus.OPERATION_COMPLETE -> {
+                ContextCompat.getColor(view.context, R.color.gray400)
+            }
+        }
+    )
 }
