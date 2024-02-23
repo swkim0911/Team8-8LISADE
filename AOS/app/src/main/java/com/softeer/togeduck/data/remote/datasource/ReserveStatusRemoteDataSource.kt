6d234dcf8cd05268ae2f1ae265d448655bb23478
YUJEONG YUN <yunyj99@naver.com>
2024-02-23 11:36:45 +0900

    Feat: 예약 현황 RemoteDataSource 생성  #110

---
+++ AOS/app/src/main/java/com/softeer/togeduck/data/remote/datasource/ReserveStatusRemoteDataSource.kt
@@ -0,0 +1,24 @@
+package com.softeer.togeduck.data.remote.datasource
+
+import com.softeer.togeduck.data.dto.response.reserve_status.ReserveStatusDetailResponse
+import com.softeer.togeduck.data.dto.response.reserve_status.ReserveStatusResponse
+import com.softeer.togeduck.data.remote.service.ReserveStatusService
+import retrofit2.Response
+import javax.inject.Inject
+
+class ReserveStatusRemoteDataSource @Inject constructor(
+    private val reserveStatusService: ReserveStatusService
+) {
+    suspend fun getReserveStatusList(
+        page: Int,
+        size: Int,
+    ): Response<ReserveStatusResponse> {
+        return reserveStatusService.getReserveStatusList(page, size)
+    }
+
+    suspend fun getReserveStatusDetail(
+        routeId: Int,
+    ): Response<ReserveStatusDetailResponse> {
+        return reserveStatusService.getReserveStatusDetail(routeId)
+    }
+}
