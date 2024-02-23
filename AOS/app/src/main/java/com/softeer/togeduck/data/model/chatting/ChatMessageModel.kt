b0c8fd92f90ed85ec081adef8ff65ab364e7795b
Hymin2 <khm8246@gmail.com>
2024-02-23 15:03:23 +0900

    Feat: ChatMessageModel PK autoGenerate 추가 #186
    - 구현 내용
        1. ChatMessageModel에서 PK에 autoGenerate를 추가했습니다.

--- AOS/app/src/main/java/com/softeer/togeduck/data/model/chatting/ChatMessageModel.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/model/chatting/ChatMessageModel.kt
@@ -14,8 +14,8 @@ import androidx.room.PrimaryKey
     )
 ])
 data class ChatMessageModel(
-    @PrimaryKey
-    val id: Long,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
     @ColumnInfo(name = "room_id")
     val roomId: Long,
     val nickname: String?,
