8dc620998bb905d702ad2377862eb5cfae5b80e2
dlwnsgus07 <dlwnsgus07@naver.com>
2024-02-24 02:15:18 +0900

    Feat : 인기 공연을 검색하는 쿼리를 변경된 DTO에 맞춰 정보 수정
    BestResponseDto의 내용이 수정됨에 따라 sql 문변경

--- BE/src/main/java/com/lisade/togeduck/repository/FestivalRepositoryCustom.java
+++ BE/src/main/java/com/lisade/togeduck/repository/FestivalRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.lisade.togeduck.repository;
 
 import com.lisade.togeduck.dto.response.BestFestivalDto;
+import com.lisade.togeduck.dto.response.FestivalResponse;
 import java.util.List;
 
 public interface FestivalRepositoryCustom {
 
     List<BestFestivalDto> findBest(Integer limit);
+
+    List<FestivalResponse> findFestivalsByCategorySortedByScore(Long categoryId, Integer limit);
 }
