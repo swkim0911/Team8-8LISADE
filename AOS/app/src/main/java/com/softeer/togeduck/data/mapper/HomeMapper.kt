abe4f453ed22509cf0f7a142d0966ae219ea51c2
ssangdon <tngustkdehs@naver.com>
2024-02-24 01:54:08 +0900

    Feat: 카테고리별 페스티벌 릭스트 받아오는 API연동#168

--- AOS/app/src/main/java/com/softeer/togeduck/data/mapper/HomeMapper.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/mapper/HomeMapper.kt
@@ -1,9 +1,9 @@
 package com.softeer.togeduck.data.mapper
 
+import com.softeer.togeduck.data.dto.response.home.HomeCategoryFestivalResponse
 import com.softeer.togeduck.data.dto.response.home.HomeCategoryResponse
-import com.softeer.togeduck.data.dto.response.home.HomePopularArticleResponse
+import com.softeer.togeduck.data.model.home.main.HomeArticleModel
 import com.softeer.togeduck.data.model.home.main.HomeCategoryModel
-import com.softeer.togeduck.data.model.home.main.PopularArticleModel
 
 
 fun HomeCategoryResponse.toHomeCategoryModel(): List<HomeCategoryModel> {
@@ -21,3 +21,15 @@ fun HomeCategoryResponse.toHomeCategoryModel(): List<HomeCategoryModel> {
 //    }
 //}
 //
+
+fun HomeCategoryFestivalResponse.toHomeArticleModel(): List<HomeArticleModel> {
+    return this.content.map { content ->
+        HomeArticleModel(
+            imgUrl = content.thumbnailPath,
+            title = content.title,
+            place = content.location,
+            startDate = content.startedAt,
+            endDate = content.startedAt,
+        )
+    }
+}
--- AOS/app/src/main/java/com/softeer/togeduck/utils/BindingAdapter.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/utils/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.softeer.togeduck.utils
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -9,5 +10,17 @@ import com.bumptech.glide.Glide
 fun loadImg(view: ImageView, url: String) {
     Glide.with(view.context)
         .load(url)
+        .into(view)
+}
+
+@BindingAdapter("s3ImageUrl")
+fun setS3ImageUrl(view:ImageView, url:String){
+    val convertedUrl = url.convertS3Url() // 확장 함수 사용
+    Log.d("TESTLOG4444",convertedUrl)
+    Glide.with(view.context)
+        // 왜 이건 이미지 뜨고
+//        .load("https://t1.daumcdn.net/cfile/tistory/2436014554FC557F2E")
+        // 이건 안뜨지?
+        .load(convertedUrl)
         .into(view)
 }
