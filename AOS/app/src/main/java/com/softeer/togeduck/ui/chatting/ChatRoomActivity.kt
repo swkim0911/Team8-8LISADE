72234d22d7f601609d0903c23e2dfca92817edfa
Hymin2 <khm8246@gmail.com>
2024-02-23 11:06:21 +0900

    Feat: 채팅방 화면 기능 구현 #186
    - 구현 내용
        1. 채팅 메세지를 데이터베이스에서 조회 후 recycler view를 통해 보여질 수 있도록 합니다.

--- AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomActivity.kt
@@ -2,11 +2,75 @@ package com.softeer.togeduck.ui.chatting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.softeer.togeduck.R
+import androidx.activity.OnBackPressedCallback
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.softeer.togeduck.data.model.chatting.ChatMessageModel
+import com.softeer.togeduck.databinding.ActivityChatRoomBinding
+import com.softeer.togeduck.room.TogeduckDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ChatRoomActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatRoomBinding
+    private var newMessages = arrayListOf<ChatMessageModel>()
+
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat_room)
+        binding = ActivityChatRoomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        this.onBackPressedDispatcher.addCallback(this, callback)
+
+        val id = intent.getLongExtra("id", 0)
+        val roomName = intent.getStringExtra("roomName")
+
+        binding.apply {
+            chatRoomName.text = roomName
+
+            chatRoomBackButton.setOnClickListener {
+                finish()
+            }
+
+            rvChatRoomMessage.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+                if (bottom < oldBottom) {
+                    rvChatRoomMessage.postDelayed({
+                        if (rvChatRoomMessage.adapter!!.itemCount > 0) {
+                            rvChatRoomMessage.scrollToPosition(rvChatRoomMessage.adapter!!.itemCount - 1)
+                        }
+                    }, 100)
+                }
+            }
+
+        }
+
+        getMessages(id)
+    }
+
+    private fun getMessages(roomId: Long){
+        val db = TogeduckDatabase.getInstance(binding.root.context)
+
+        binding.apply {
+            CoroutineScope(Dispatchers.IO).launch {
+                val messages = db!!.chatMessageDao().getMessagesByRoomId(roomId) as ArrayList<ChatMessageModel>
+
+                val adapter = ChatMessageListAdapter(messages)
+
+                withContext(Dispatchers.Main) {
+                    rvChatRoomMessage.layoutManager = LinearLayoutManager(this@ChatRoomActivity,
+                        LinearLayoutManager.VERTICAL,
+                        false)
+                    rvChatRoomMessage.adapter = adapter
+                    rvChatRoomMessage.scrollToPosition(messages.size - 1)
+                }
+            }
+        }
     }
 }
