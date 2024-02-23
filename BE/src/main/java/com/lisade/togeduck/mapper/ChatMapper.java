69cb9b8d20f38783a3a838a9ddee3c18059486ca
Hymin2 <khm8246@gmail.com>
2024-02-24 04:26:17 +0900

    Feat: ChatMapper 함수 추가 #198
    - 구현 내용
        1. ChatJoinRequest -> ChatMessageResponse 매핑 함수를 추가했습니다.
        2. ChatMessageRequest -> ChatMessageResponse 매핑 함수를 추가했습니다.

--- BE/src/main/java/com/lisade/togeduck/mapper/ChatMapper.java
+++ BE/src/main/java/com/lisade/togeduck/mapper/ChatMapper.java
@@ -1,17 +1,42 @@
 package com.lisade.togeduck.mapper;
 
+import com.lisade.togeduck.dto.request.ChatJoinRequest;
 import com.lisade.togeduck.dto.request.ChatMessageRequest;
+import com.lisade.togeduck.dto.response.ChatMessageResponse;
 import com.lisade.togeduck.entity.ChatMessage;
+import com.lisade.togeduck.entity.User;
+import com.lisade.togeduck.entity.enums.MessageAction;
 
 public class ChatMapper {
 
-    public static ChatMessage toChatMessage(ChatMessageRequest chatMessageRequest) {
+    public static ChatMessage toChatMessage(User user, ChatMessageResponse chatMessageResponse) {
         return ChatMessage.builder()
-            .userId(chatMessageRequest.getUserId())
-            .roomId(chatMessageRequest.getRoomId())
-            .sender(chatMessageRequest.getSender())
-            .message(chatMessageRequest.getMessage())
-            .action(chatMessageRequest.getAction())
-            .createdAt(chatMessageRequest.getCreatedAt()).build();
+            .roomId(chatMessageResponse.getRoomId())
+            .sender(user.getNickname())
+            .message(chatMessageResponse.getMessage())
+            .action(MessageAction.getMessageAction(chatMessageResponse.getAction()))
+            .createdAt(chatMessageResponse.getCreatedAt()).build();
+    }
+
+    public static ChatMessageResponse toChatMessageResponse(User user,
+        ChatMessageRequest chatMessageResponse) {
+        return ChatMessageResponse.builder()
+            .roomId(chatMessageResponse.getRoomId())
+            .sender(user.getNickname())
+            .message(chatMessageResponse.getMessage())
+            .createdAt(chatMessageResponse.getCreatedAt())
+            .action(chatMessageResponse.getAction())
+            .build();
+    }
+
+    public static ChatMessageResponse toChatMessageResponse(User user,
+        ChatJoinRequest chatJoinRequest, String message) {
+        return ChatMessageResponse.builder()
+            .roomId(chatJoinRequest.getRoomId())
+            .sender(user.getNickname())
+            .message(message)
+            .createdAt(chatJoinRequest.getCreatedAt())
+            .action(MessageAction.JOIN.name())
+            .build();
     }
 }
