d0bf91d52ede85aed7b370fbabe8d17b6e0a741c
YUJEONG YUN <yunyj99@naver.com>
2024-02-24 23:32:36 +0900

    Refactor: 좌석 선택 커스텀 뷰에서 라이브 데이터 대신 클릭 함수로 선택 여부 확인하도록 수정 #205

--- AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatActivity.kt
@@ -3,7 +3,9 @@ package com.softeer.togeduck.ui.home.seat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.softeer.togeduck.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SeatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatSelectFragment.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/home/seat/SeatSelectFragment.kt
@@ -1,15 +1,18 @@
 package com.softeer.togeduck.ui.home.seat
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.softeer.togeduck.R
 import com.softeer.togeduck.databinding.FragmentSeatSelectBinding
+import com.softeer.togeduck.utils.ItemClickWithSeat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,5 +39,27 @@ class SeatSelectFragment : Fragment() {
         binding.selectCompleteBtn.setOnClickListener {
             findNavController().navigate(R.id.action_seatSelectFragment_to_seatPaymentFragment)
         }
+        binding.seatSelectView.itemClick = object : ItemClickWithSeat {
+            override fun onClick(existSelectedSeat: Boolean) {
+                binding.selectCompleteBtn.isEnabled = existSelectedSeat
+                if (existSelectedSeat) {
+                    binding.selectCompleteBtn.setBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.main
+                        )
+                    )
+                } else {
+                    binding.selectCompleteBtn.setBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.gray300
+                        )
+                    )
+                }
+
+            }
+        }
+
     }
 }
