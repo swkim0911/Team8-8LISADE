a79caf3ea49eb51c8549cbe501dffac28babd1f1
Hymin2 <khm8246@gmail.com>
2024-02-24 21:13:39 +0900

    Feat: ChannelInboundInterceptor 생성 및 구현 #198
    - 구현 내용
        1. 채팅방에 들어올 때 FcmToken을 제거하도록 구현했습니다.
        2. 채팅방에 나가면 FcmToken을 추가하도록 구현했습니다.
        3. 채팅방 세션에 FcmToken이 존재하는 사용자에게 푸시 알림을 보내도록 했습니다.

--- BE/src/main/java/com/lisade/togeduck/Interceptor/ChannelInboundInterceptor.java
+++ BE/src/main/java/com/lisade/togeduck/Interceptor/ChannelInboundInterceptor.java
@@ -1,20 +1,59 @@
 package com.lisade.togeduck.Interceptor;
 
+import static com.lisade.togeduck.constant.SessionConst.LOGIN_USER;
+
+import com.lisade.togeduck.cache.ChatRoomSessionCacheService;
+import com.lisade.togeduck.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class ChannelInboundInterceptor implements ChannelInterceptor {
+
+    private final ChatRoomSessionCacheService chatRoomSessionCacheService;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        System.out.println("header.getCommand() = " + accessor.getCommand());
-        System.out.println("header.getDestination() = " + accessor.getDestination());
-        System.out.println("accessor.getSessionId() = " + accessor.getSessionId());
+
         return message;
+    }
+
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(
+            RequestContextHolder.getRequestAttributes())).getRequest();
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute(LOGIN_USER.getSessionName());
+
+        log.info(message.getHeaders().get("simpDestination").toString());
+        log.info(user.getNickname());
+
+        String dest = String.valueOf(message.getHeaders().get("simpDestination"));
+
+        if (Objects.requireNonNull(accessor.getCommand()) == StompCommand.SUBSCRIBE) {
+            String fcmToken = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
+            chatRoomSessionCacheService.deleteSession(dest, fcmToken);
+
+        } else if (Objects.requireNonNull(accessor.getCommand()) == StompCommand.DISCONNECT) {
+            String fcmToken = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
+
+            chatRoomSessionCacheService.addSession(dest, fcmToken);
+        }
     }
 }
