2d963bfee192383d0ed9b12f29748149e18c9f70
Hymin2 <khm8246@gmail.com>
2024-02-25 08:05:51 +0900

    Feat: ChannelInboundInterceptor 수정 #198
    - 구현 내용
        1. 채팅 메시지 전송 시 sessionId를 통해서 메시지에 sender를 추가하도록 구현했습니다.

--- BE/src/main/java/com/lisade/togeduck/Interceptor/ChannelInboundInterceptor.java
+++ BE/src/main/java/com/lisade/togeduck/Interceptor/ChannelInboundInterceptor.java
@@ -1,12 +1,10 @@
 package com.lisade.togeduck.Interceptor;
 
-import static com.lisade.togeduck.constant.SessionConst.LOGIN_USER;
-
-import com.lisade.togeduck.cache.ChatRoomSessionCacheService;
-import com.lisade.togeduck.entity.User;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import com.lisade.togeduck.cache.service.ChatRoomSessionCacheService;
+import com.lisade.togeduck.cache.service.SessionCacheService;
+import com.lisade.togeduck.cache.value.SessionCacheValue;
 import java.util.Objects;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -14,20 +12,39 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChannelInboundInterceptor implements ChannelInterceptor {
 
+    private final SessionCacheService sessionCacheService;
     private final ChatRoomSessionCacheService chatRoomSessionCacheService;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String payload = new String((byte[]) message.getPayload());
+
+        if (!payload.isEmpty()) {
+            String session = accessor.getFirstNativeHeader("Cookie");
+            Optional<SessionCacheValue> sessionCacheValue = sessionCacheService.getBySession(
+                session);
+
+            if (sessionCacheValue.isPresent()) {
+                StringBuilder newPayload = new StringBuilder(payload);
+                String additionMessage =
+                    ",\"sender\":\"" + sessionCacheValue.get().getNickname() + "\"}";
+                newPayload.deleteCharAt(newPayload.length() - 1);
+                newPayload.append(additionMessage);
+
+                return MessageBuilder.withPayload(newPayload.toString())
+                    .copyHeaders(message.getHeaders())
+                    .build();
+            }
+        }
 
         return message;
     }
@@ -36,24 +53,10 @@ public class ChannelInboundInterceptor implements ChannelInterceptor {
     public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(
-            RequestContextHolder.getRequestAttributes())).getRequest();
-        HttpSession session = request.getSession(false);
-        User user = (User) session.getAttribute(LOGIN_USER.getSessionName());
-
-        log.info(message.getHeaders().get("simpDestination").toString());
-        log.info(user.getNickname());
-
-        String dest = String.valueOf(message.getHeaders().get("simpDestination"));
-
         if (Objects.requireNonNull(accessor.getCommand()) == StompCommand.SUBSCRIBE) {
-            String fcmToken = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
-            chatRoomSessionCacheService.deleteSession(dest, fcmToken);
 
         } else if (Objects.requireNonNull(accessor.getCommand()) == StompCommand.DISCONNECT) {
-            String fcmToken = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
 
-            chatRoomSessionCacheService.addSession(dest, fcmToken);
         }
     }
 }
