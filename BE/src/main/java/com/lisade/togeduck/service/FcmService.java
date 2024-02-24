39498f29d9920129739a70afb1887af277b117ae
Hymin2 <khm8246@gmail.com>
2024-02-24 20:43:38 +0900

    Feat: FcmService 생성 및 구현 #198
    - 구현 내용
        1. FirebaseMessaging과 관련된 기능을 수헹하는 FcmService를 생성했습니다.
        2. FcmToken을 저장하는 함수를 구현했습니다.
        3. 푸시 알림을 보내는 함수를 구현했습니다.

---
+++ BE/src/main/java/com/lisade/togeduck/service/FcmService.java
@@ -0,0 +1,37 @@
+package com.lisade.togeduck.service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import com.lisade.togeduck.cache.FcmTokenCacheService;
+import com.lisade.togeduck.dto.request.FcmTokenRequest;
+import com.lisade.togeduck.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FcmService {
+
+    private final FirebaseMessaging firebaseMessaging;
+    private final FcmTokenCacheService fcmTokenCacheService;
+
+    public void save(User user, FcmTokenRequest fcmTokenRequest) {
+        fcmTokenCacheService.save(user.getNickname(), fcmTokenRequest.getFcmToken());
+    }
+
+    public void sendNotification(String title, String message, String fcmToken)
+        throws FirebaseMessagingException {
+        firebaseMessaging.send(
+            Message.builder()
+                .setNotification(
+                    Notification.builder()
+                        .setTitle(title)
+                        .setBody(message)
+                        .build()
+                ).setToken(fcmToken)
+                .build()
+        );
+    }
+}
