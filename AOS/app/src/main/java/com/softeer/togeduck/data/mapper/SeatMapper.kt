411da66ed59345b18c09dd6278e272aca16fa040
YUJEONG YUN <yunyj99@naver.com>
2024-02-24 22:49:00 +0900

    Refactor: SeatInfoModel에서 seats는 매진된 좌석값만 갖도록 수정 #205

--- AOS/app/src/main/java/com/softeer/togeduck/data/mapper/SeatMapper.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/mapper/SeatMapper.kt
@@ -1,13 +1,11 @@
 package com.softeer.togeduck.data.mapper
 
 import com.softeer.togeduck.data.dto.response.home.seat.SeatsInfoResponse
-import com.softeer.togeduck.data.model.home.seat.SeatStatusModel
 import com.softeer.togeduck.data.model.home.seat.SeatsInfoModel
 import com.softeer.togeduck.utils.addCommas
 
 fun SeatsInfoResponse.toSeatInfoModel(): SeatsInfoModel {
-    return SeatsInfoModel(
-        seatsCntPerRow = row,
+    return SeatsInfoModel(seatsCntPerRow = row,
         totalRows = column,
         backSeatsCnt = backSeat,
         totalSeats = totalSeats,
@@ -16,12 +14,5 @@ fun SeatsInfoResponse.toSeatInfoModel(): SeatsInfoModel {
         arrivalPlace = "${destinationCity}(${destination})",
         price = price,
         formattedPrice = price.addCommas(),
-        seats = seats.map { seat ->
-            SeatStatusModel(
-                id = seat.id,
-                seatNo = seat.seatNo,
-                status = seat.status
-            )
-        }
-    )
+        seats = seats.filter { !it.status.isAvailable }.map { it.seatNo.toInt() })
 }
