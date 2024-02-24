ddc4f0d7e01d5a72b4c79051616960bf6aaa9de2
YUJEONG YUN <yunyj99@naver.com>
2024-02-24 22:07:09 +0900

    Feat: 좌석 선택 Response 객체 생성  #205

---
+++ AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/home/seat/SeatsInfoResponse.kt
@@ -0,0 +1,39 @@
+package com.softeer.togeduck.data.dto.response.home.seat
+
+import com.squareup.moshi.Json
+
+data class SeatResponse(
+    @Json(name = "numberOfSeats")
+    val numberOfSeats: Int,
+    @Json(name = "row")
+    val row: Int,
+    @Json(name = "column")
+    val column: Int,
+    @Json(name = "backSeat")
+    val backSeat: Int,
+    @Json(name = "totalSeats")
+    val totalSeats: Int,
+    @Json(name = "reservedSeats")
+    val reservedSeats: Int,
+    @Json(name = "sourceCity")
+    val sourceCity: String,
+    @Json(name = "source")
+    val source: String,
+    @Json(name = "destinationCity")
+    val destinationCity: String,
+    @Json(name = "destination")
+    val destination: String,
+    @Json(name = "price")
+    val price: Int,
+    @Json(name = "seats")
+    val seats: List<SeatStatusResponse>,
+)
+
+data class SeatStatusResponse(
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "seatNo")
+    val seatNo: String,
+    @Json(name = "status")
+    val status: String,
+)
