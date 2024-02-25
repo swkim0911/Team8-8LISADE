8f2725c6136e652d266dbc50c616b61d2098d74e
Hymin2 <khm8246@gmail.com>
2024-02-25 23:14:43 +0900

    Refactor: ChatRoomListFragment MVVM 패턴 적용  #195
    - 구현 내용
        1. ChatRoomListFragment와 MVVM 패턴을 적용하여 Fragment와 ViewModel로 분리했습니다.

--- AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomListFragment.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomListFragment.kt
@@ -2,21 +2,19 @@ package com.softeer.togeduck.ui.chatting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.softeer.togeduck.data.model.chatting.ChatRoomListModel
 import com.softeer.togeduck.databinding.FragmentChatRoomListBinding
-import com.softeer.togeduck.room.TogeduckDatabase
 import com.softeer.togeduck.utils.ItemClick
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class ChatRoomListFragment : Fragment() {
     private lateinit var binding: FragmentChatRoomListBinding
+    private lateinit var viewModel: ChatRoomListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,48 +26,37 @@ class ChatRoomListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[ChatRoomListViewModel::class.java]
+        initObservers()
     }
 
     override fun onResume() {
         super.onResume()
-        init()
+        viewModel.loadChatRooms()
     }
 
-    private fun init() {
-        val db = TogeduckDatabase.getInstance(binding.root.context)
+    private fun initObservers() {
+        viewModel.chatRooms.observe(viewLifecycleOwner) { chatRooms ->
+            val adapter = ChatRoomListAdapter(chatRooms)
+            binding.rvChatRoomList.adapter = adapter
 
-        binding.apply {
-            CoroutineScope(Dispatchers.IO).launch {
-                val chatRooms = db!!.chatRoomsDao().getAll()
-                val adapter = ChatRoomListAdapter(chatRooms)
-
-                withContext(Dispatchers.Main){
-                    rvChatRoomList.adapter = adapter
-
-                    adapter.itemClick = object : ItemClick {
-                        override fun onClick(view: View, position: Int) {
-                            val intent = Intent(binding.root.context, ChatRoomActivity::class.java)
-
-                            intent.putExtra("id", chatRooms[position].id)
-                            intent.putExtra("roomName", chatRooms[position].roomName)
-
-                            setUnreadMessageCountZero(chatRooms[position])
-
-                            startActivity(intent)
-                        }
+            adapter.itemClick = object : ItemClick {
+                override fun onClick(view: View, position: Int) {
+                    val chatRoom = chatRooms[position]
+                    val intent = Intent(requireContext(), ChatRoomActivity::class.java).apply {
+                        putExtra("id", chatRoom.id)
+                        putExtra("roomName", chatRoom.roomName)
                     }
+
+                    setUnreadMessageCountZero(chatRoom)
+                    startActivity(intent)
                 }
             }
         }
     }
 
-    private fun setUnreadMessageCountZero(chatRoomListModel: ChatRoomListModel){
-        val db = TogeduckDatabase.getInstance(binding.root.context)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            chatRoomListModel.unreadMessageCount = 0
-
-            db!!.chatRoomsDao().update(chatRoomListModel)
-        }
+    private fun setUnreadMessageCountZero(chatRoomListModel: ChatRoomListModel) {
+        viewModel.updateChatRoom(chatRoomListModel.copy(unreadMessageCount = 0))
     }
 }
