721f74f764f97eb52596bdd154f1abb8a6ce6b6c
ssangdon <tngustkdehs@naver.com>
2024-02-26 01:08:01 +0900

    Feat: 출발 장소 리스트 조회 API연동#212

--- AOS/app/src/main/java/com/softeer/togeduck/data/model/home/open_route/RegionDetailModel.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/model/home/open_route/RegionDetailModel.kt
@@ -1,5 +1,6 @@
 package com.softeer.togeduck.data.model.home.open_route
 
 data class RegionDetailModel(
+    val id : Int,
     val detail : String
 )
--- AOS/app/src/main/java/com/softeer/togeduck/data/model/home/open_route/RegionListModel.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/model/home/open_route/RegionListModel.kt
@@ -2,6 +2,7 @@ package com.softeer.togeduck.data.model.home.open_route
 
 
 data class RegionListModel(
+    val id: Int,
     val region : String,
-    val detailList: List<RegionDetailModel>
+    val detailList: List<RegionDetailModel>,
 )
