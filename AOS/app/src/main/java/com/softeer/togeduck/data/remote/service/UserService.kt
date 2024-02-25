a58bc09f18bff2659b58c2bdb58cb64d6f39995d
Hymin2 <khm8246@gmail.com>
2024-02-25 23:16:21 +0900

    Feat: FcmToken 서버 등록 API 요청 함수 추가 #195
    - 구현 내용
        1. 서버에 FcmToken을 저장 요청을 하는 API 요청 함수를 추가했습니다.

--- AOS/app/src/main/java/com/softeer/togeduck/data/remote/service/UserService.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/remote/service/UserService.kt
@@ -1,5 +1,6 @@
 package com.softeer.togeduck.data.remote.service
 
+import com.softeer.togeduck.data.dto.request.FcmTokenRequest
 import com.softeer.togeduck.data.dto.request.LoginRequest
 import retrofit2.Response
 import retrofit2.http.Body
@@ -9,5 +10,10 @@ interface UserService {
     @POST("users/login")
     suspend fun login(
         @Body loginRequest: LoginRequest,
+    ): Response<String>
+
+    @POST("fcm")
+    suspend fun sendFcmToken(
+        @Body fcmTokenRequest: FcmTokenRequest
     ): Response<String>
 }
