64d506c3a81162e36f60ffc397e949517430f08b
Hymin2 <khm8246@gmail.com>
2024-02-25 23:11:56 +0900

    Refactor: ChatActivity MVVM 패턴 적용  #195
    - 구현 내용
        1. ChatActivity를 MVVM 패턴을 적용하여 Activity와 ViewModel로 분리했습니다.

--- AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomActivity.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomActivity.kt
@@ -1,38 +1,23 @@
 package com.softeer.togeduck.ui.chatting
 
-import android.annotation.SuppressLint
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.ViewModelProvider
 import com.softeer.togeduck.data.model.chatting.ChatMessageModel
 import com.softeer.togeduck.databinding.ActivityChatRoomBinding
-import com.softeer.togeduck.room.TogeduckDatabase
-import com.softeer.togeduck.utils.TimeFormatter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import ua.naiksoftware.stomp.Stomp
-import ua.naiksoftware.stomp.dto.LifecycleEvent
-import java.util.UUID
 
 class ChatRoomActivity: AppCompatActivity() {
     private lateinit var binding: ActivityChatRoomBinding
-
-    private var newMessages = arrayListOf<ChatMessageModel>()
-    private var id = 0L
-
-    private val url = "ws://10.0.2.2:8080/chat"
-    private val stomp = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
-    private val uuid = UUID.randomUUID().toString()
+    private lateinit var viewModel: ChatRoomViewModel
 
     private val callback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -46,10 +31,17 @@ class ChatRoomActivity: AppCompatActivity() {
         binding = ActivityChatRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this)[ChatRoomViewModel::class.java]
+
+        val id = intent.getLongExtra("id", 0)
+        val roomName = intent.getStringExtra("roomName")
+
         this.onBackPressedDispatcher.addCallback(this, callback)
 
-        id = intent.getLongExtra("id", 0)
-        val roomName = intent.getStringExtra("roomName")
+        viewModel.init(id)
+
+        observeChatMessages()
+        observeNewChatMessage()
 
         binding.apply {
             setSupportActionBar(chatRoomToolbar)
@@ -72,224 +64,50 @@ class ChatRoomActivity: AppCompatActivity() {
             }
 
             chatMessageSendBtn.setOnClickListener {
-                if (chatMessageEditText.text.isEmpty()) {
+                val message = chatMessageEditText.text.toString()
+
+                if (message.isNotEmpty()) {
+                    viewModel.sendMessage(message)
+                    chatMessageEditText.text.clear()
+                } else {
                     chatMessageEditText.requestFocus()
                     keyBordShow()
                     Toast.makeText(this@ChatRoomActivity, "메세지를 입력하세요", Toast.LENGTH_SHORT).show()
-                } else {
-                    sendMessage(chatMessageEditText.text.toString())
-                    chatMessageEditText.text.clear()
-                    rvChatRoomMessage.scrollToPosition(rvChatRoomMessage.adapter!!.itemCount - 1)
                 }
             }
-        }
-
-        getMessages()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        stomp.disconnect()
-
-        if(newMessages.isNotEmpty()) {
-            updateChatRoom()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        stomp.disconnect()
-
-        if(newMessages.isNotEmpty()) {
-            insertMessages()
-            updateChatRoom()
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         super.onResume()
-        runStomp()
-        readMessage()
+        viewModel.connectToStomp()
+        viewModel.readMessage()
     }
 
     private fun keyBordShow() {
         WindowInsetsControllerCompat(window, window.decorView).show(WindowInsetsCompat.Type.ime())
     }
 
-    private fun getMessages() {
-        val db = TogeduckDatabase.getInstance(binding.root.context)
+    private fun observeChatMessages() {
+        viewModel.chatMessages.observe(this) { messages ->
+            binding.apply {
+                val adapter = ChatMessageListAdapter(messages as ArrayList<ChatMessageModel>)
 
-        binding.apply {
-            CoroutineScope(Dispatchers.IO).launch {
-                val messages =
-                    db!!.chatMessageDao().getMessagesByRoomId(id) as ArrayList<ChatMessageModel>
-
-                val adapter = ChatMessageListAdapter(messages)
-
-                withContext(Dispatchers.Main) {
-                    rvChatRoomMessage.layoutManager = LinearLayoutManager(
-                        this@ChatRoomActivity,
-                        LinearLayoutManager.VERTICAL,
-                        false
-                    )
-                    rvChatRoomMessage.adapter = adapter
-                    rvChatRoomMessage.scrollToPosition(messages.size - 1)
-                }
+                rvChatRoomMessage.adapter = adapter
+                rvChatRoomMessage.scrollToPosition(rvChatRoomMessage.adapter!!.itemCount - 1)
             }
         }
     }
 
-    @SuppressLint("CheckResult")
-    private fun runStomp() {
-        stomp.lifecycle().subscribe { lifecycle ->
-            when (lifecycle.type) {
-                LifecycleEvent.Type.OPENED -> {
-                    Log.i("OPEND", "!!")
-                }
+    private fun observeNewChatMessage() {
+        viewModel.newMessages.observe(this) { messages ->
+            binding.apply {
+                val adapter = rvChatRoomMessage.adapter as ChatMessageListAdapter
 
-                LifecycleEvent.Type.CLOSED -> {
-                    Log.i("CLOSED", "!!")
-
-                }
-
-                LifecycleEvent.Type.ERROR -> {
-                    Log.i("ERROR", "!!")
-                    Log.e("CONNECT ERROR", lifecycle.exception.toString())
-                }
-
-                else -> {
-                    Log.i("ELSE", lifecycle.message)
-                }
+                adapter.addItem(messages.last())
+                rvChatRoomMessage.scrollToPosition(rvChatRoomMessage.adapter!!.itemCount - 1)
             }
-        }
-
-        stomp.connect()
-    }
-
-    @SuppressLint("CheckResult")
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun readMessage() {
-        stomp.topic("/topic/message/$id").subscribe { stompMessage ->
-            val data = JSONObject(stompMessage.payload)
-
-            val uuid = data.get("uuid") as String
-
-            if (this.uuid != uuid) {
-                val nickname = data.get("sender") as String
-                val message = data.get("message") as String
-                val time = data.get("createdAt") as String
-                val type = data.get("action") as String
-
-                var location = ""
-
-                var lastMessage = getLastMessage()
-
-                if (type == "JOIN" || type == "LEAVE") {
-                    location = "CENTER"
-                } else if (lastMessage == null || nickname != lastMessage.nickname) {
-                    location = "FIRST_LEFT"
-                } else {
-                    location = "LEFT"
-                }
-
-                runOnUiThread {
-                    addTimeMessage(time)
-                    updateMessage(nickname, message, time, type, location)
-                    binding.rvChatRoomMessage.scrollToPosition(binding.rvChatRoomMessage.adapter!!.itemCount - 1)
-                }
-            }
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun sendMessage(message: String) {
-        val data = JSONObject()
-        val now = TimeFormatter.now()
-
-        data.put("roomId", id)
-        data.put("message", message)
-        data.put("createdAt", now)
-        data.put("action", "MESSAGE")
-        data.put("uuid", uuid)
-
-        stomp.send("/app/chat/message", data.toString()).subscribe()
-
-        addTimeMessage(now)
-        updateMessage("!", message, now, "MESSAGE", "RIGHT")
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun addTimeMessage(now: String) {
-        val lastMessage = getLastMessage()
-
-        if (lastMessage == null) {
-            updateMessage("", TimeFormatter.yyyyMMddE(now), now, "DATE", "CENTER")
-
-            return
-        }
-
-        val lastMessageDate = TimeFormatter.toLocalDateTime(lastMessage.time!!).toLocalDate()
-        val nowDate = TimeFormatter.toLocalDateTime(now).toLocalDate()
-
-        if (!lastMessageDate.isEqual(nowDate)) {
-            updateMessage("", TimeFormatter.yyyyMMddE(now), now, "DATE", "CENTER")
-        }
-    }
-
-    private fun isFirstMessage(): Boolean {
-        binding.apply {
-            val adapter = rvChatRoomMessage.adapter as ChatMessageListAdapter
-
-            return adapter.itemCount == 0
-        }
-    }
-
-    private fun getLastMessage(): ChatMessageModel? {
-        binding.apply {
-            if (isFirstMessage()){
-                return null
-            }
-
-            val adapter = rvChatRoomMessage.adapter as ChatMessageListAdapter
-
-            return adapter.getLastItem()
-        }
-    }
-
-    private fun updateMessage(nickname: String, message: String, time: String, type: String, location: String) {
-        val newChatMessageModel = ChatMessageModel(0, id, nickname, message, time, type, location)
-
-        addMessageInRecyclerView(newChatMessageModel)
-        newMessages.add(newChatMessageModel)
-    }
-
-    private fun addMessageInRecyclerView(chatMessageModel: ChatMessageModel) {
-        binding.apply {
-            val adapter = rvChatRoomMessage.adapter as ChatMessageListAdapter
-
-            adapter.addItem(chatMessageModel)
-        }
-    }
-
-    private fun insertMessages() {
-        val db = TogeduckDatabase.getInstance(binding.root.context)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            db!!.chatMessageDao().insertAll(newMessages)
-        }
-    }
-
-    private fun updateChatRoom() {
-        val db = TogeduckDatabase.getInstance(binding.root.context)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val chatRoomListModel = db!!.chatRoomsDao().get(id)
-
-            chatRoomListModel.recentTime = newMessages[newMessages.size - 1].time!!
-            chatRoomListModel.recentMessage = newMessages[newMessages.size - 1].message
-
-            db.chatRoomsDao().update(chatRoomListModel)
         }
     }
 }
---
+++ AOS/app/src/main/java/com/softeer/togeduck/ui/chatting/ChatRoomViewModel.kt
@@ -0,0 +1,205 @@
+package com.softeer.togeduck.ui.chatting
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.softeer.togeduck.data.local.datasource.UserDataStore
+import com.softeer.togeduck.data.model.chatting.ChatMessageModel
+import com.softeer.togeduck.data.repository.ChatMessageRepository
+import com.softeer.togeduck.data.repository.ChatRoomListRepository
+import com.softeer.togeduck.utils.TimeFormatter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.dto.LifecycleEvent
+import ua.naiksoftware.stomp.dto.StompHeader
+import java.util.UUID
+
+
+class ChatRoomViewModel(application: Application) : AndroidViewModel(application) {
+    private val chatMessageRepository = ChatMessageRepository(application)
+    private val chatRoomListRepository = ChatRoomListRepository(application)
+    private val userDataStore = UserDataStore(application)
+
+    private val _chatMessages = MutableLiveData<List<ChatMessageModel>>()
+    val chatMessages: LiveData<List<ChatMessageModel>> get() = _chatMessages
+
+    private var _newMessages = MutableLiveData<ArrayList<ChatMessageModel>>()
+    val newMessages: LiveData<ArrayList<ChatMessageModel>> get() = _newMessages
+
+    private var id = 0L
+
+    private val url = "ws://10.0.2.2:8080/chat"
+    private val stomp = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
+    private val uuid = UUID.randomUUID().toString()
+
+    fun init(id: Long) {
+        this.id = id
+        loadChatMessages(id)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        stomp.disconnect()
+
+        if (newMessages.value!!.isNotEmpty()) {
+            insertNewMessages()
+            updateChatRoom()
+        }
+    }
+
+    private fun loadChatMessages(id: Long) {
+        viewModelScope.launch {
+            val messages = chatMessageRepository.getMessagesByRoomId(id)
+            _chatMessages.postValue(messages)
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    fun connectToStomp() {
+        stomp.lifecycle().subscribe { lifecycle ->
+            when (lifecycle.type) {
+                LifecycleEvent.Type.OPENED -> {
+                    Log.i("OPEND", "!!")
+                }
+
+                LifecycleEvent.Type.CLOSED -> {
+                    Log.i("CLOSED", "!!")
+
+                }
+
+                LifecycleEvent.Type.ERROR -> {
+                    Log.i("ERROR", "!!")
+                    Log.e("CONNECT ERROR", lifecycle.exception.toString())
+                }
+
+                else -> {
+                    Log.i("ELSE", lifecycle.message)
+                }
+            }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val session = userDataStore.getUserSessionId.first()
+
+            withContext(Dispatchers.Main) {
+                val headerList = ArrayList<StompHeader>()
+                headerList.add(StompHeader("Cookie", session))
+                stomp.connect(headerList)
+            }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun readMessage() {
+        stomp.topic("/topic/message/$id").subscribe { stompMessage ->
+            val data = JSONObject(stompMessage.payload)
+
+            val uuid = data.get("uuid") as String
+
+            if(this.uuid != uuid) {
+                val nickname = data.get("sender") as String
+                val message = data.get("message") as String
+                val time = data.get("createdAt") as String
+                val type = data.get("action") as String
+
+                var location = ""
+
+                val lastMessage = getLastItem()
+
+                if (type == "JOIN" || type == "LEAVE") {
+                    location = "CENTER"
+                } else if (lastMessage == null || nickname != lastMessage.nickname) {
+                    location = "FIRST_LEFT"
+                } else {
+                    location = "LEFT"
+                }
+
+                val newChatMessageModel =
+                    ChatMessageModel(0, id, nickname, message, time, type, location)
+
+                addTimeMessage(time)
+                addNewMessage(newChatMessageModel)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun sendMessage(message: String) {
+        val data = JSONObject()
+        val now = TimeFormatter.now()
+
+        data.put("roomId", id)
+        data.put("message", message)
+        data.put("createdAt", now)
+        data.put("action", "MESSAGE")
+        data.put("uuid", uuid)
+
+        stomp.send("/app/chat/message", data.toString()).subscribe()
+
+        val newChatMessageModel = ChatMessageModel(0, id, "!", message, now, "MESSAGE", "RIGHT")
+
+        addTimeMessage(now)
+        addNewMessage(newChatMessageModel)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun addTimeMessage(now: String) {
+        val lastMessage = getLastItem()
+
+        if (lastMessage == null) {
+            val newChatMessageModel = ChatMessageModel(0, id, "", TimeFormatter.yyyyMMddE(now), now, "DATE", "CENTER")
+            addNewMessage(newChatMessageModel)
+
+            return
+        }
+
+        val lastMessageDate = TimeFormatter.toLocalDateTime(lastMessage.time!!).toLocalDate()
+        val nowDate = TimeFormatter.toLocalDateTime(now).toLocalDate()
+
+        if (!lastMessageDate.isEqual(nowDate)) {
+            val newChatMessageModel = ChatMessageModel(0, id, "", TimeFormatter.yyyyMMddE(now), now, "DATE", "CENTER")
+            addNewMessage(newChatMessageModel)
+        }
+    }
+
+    private fun getLastItem(): ChatMessageModel? {
+        return _chatMessages.value?.lastOrNull()
+    }
+
+    private fun addNewMessage(message: ChatMessageModel) {
+        val currentMessages = _newMessages.value ?: arrayListOf()
+        currentMessages.add(message)
+        _newMessages.postValue(currentMessages)
+    }
+
+    private fun insertNewMessages() {
+        viewModelScope.launch(Dispatchers.IO) {
+            chatMessageRepository.insertAll(newMessages.value!!)
+        }
+    }
+
+    private fun updateChatRoom() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val chatRoomListModel = chatRoomListRepository.get(id)
+
+            if(chatRoomListModel != null) {
+                chatRoomListModel.recentTime = newMessages.value!!.last().time!!
+                chatRoomListModel.recentMessage = newMessages.value!!.last().message
+
+                chatRoomListRepository.update(chatRoomListModel)
+            }
+        }
+    }
+}
