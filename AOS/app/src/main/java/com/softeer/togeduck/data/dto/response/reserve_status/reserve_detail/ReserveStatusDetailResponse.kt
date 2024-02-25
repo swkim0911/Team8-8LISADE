02381d124722cd38bfac117b4ac771c71cc48dbf
ssangdon <tngustkdehs@naver.com>
2024-02-25 21:17:51 +0900

    Fix: 리뷰 피드백 반영#204

--- AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/reserve_status/reserve_detail/ReserveStatusDetailResponse.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/reserve_status/reserve_detail/ReserveStatusDetailResponse.kt
@@ -3,37 +3,21 @@ package com.softeer.togeduck.data.dto.response.reserve_status.reserve_detail
 import com.squareup.moshi.Json
 
 data class ReserveStatusDetailResponse(
-    @Json(name = "festivalId")
     val festivalId: Int,
-    @Json(name = "title")
     val title: String,
-    @Json(name = "seatNo")
     val seatNo: Int,
-    @Json(name = "expectedDuration")
     val expectedDuration: String,
-    @Json(name = "status")
     val status: ReserveStatus,
-    @Json(name = "totalSeats")
     val totalSeats: Int,
-    @Json(name = "reservedSeats")
     val reservedSeats: Int,
-    @Json(name = "price")
     val price: Int,
-    @Json(name = "imagePath")
     val imagePath: String,
-    @Json(name = "startedAt")
     val startedAt: String,
-    @Json(name = "arrivedAt")
     val arrivedAt: String,
-    @Json(name = "stationName")
     val stationName: String,
-    @Json(name = "stationCity")
     val stationCity: String,
-    @Json(name = "festivalLocation")
     val festivalLocation: String,
-    @Json(name = "festivalCity")
     val festivalCity: String,
-    @Json(name = "driverInfo")
     val driverInfo: DriverInfoResponse,
 )
 
