59584dd40735e6e8e7fb6ad6fa5d814119788b6d
YUJEONG YUN <yunyj99@naver.com>
2024-02-26 03:09:28 +0900

    Refactor: Json 애노테이션 삭제 #205

--- AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/home/seat/SeatsInfoResponse.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/home/seat/SeatsInfoResponse.kt
@@ -1,38 +1,22 @@
 package com.softeer.togeduck.data.dto.response.home.seat
 
-import com.squareup.moshi.Json
-
 data class SeatsInfoResponse(
-    @Json(name = "row")
     val row: Int,
-    @Json(name = "column")
     val column: Int,
-    @Json(name = "backSeat")
     val backSeat: Int,
-    @Json(name = "totalSeats")
     val totalSeats: Int,
-    @Json(name = "reservedSeats")
     val reservedSeats: Int,
-    @Json(name = "sourceCity")
     val sourceCity: String,
-    @Json(name = "source")
     val source: String,
-    @Json(name = "destinationCity")
     val destinationCity: String,
-    @Json(name = "destination")
     val destination: String,
-    @Json(name = "price")
     val price: Int,
-    @Json(name = "seats")
     val seats: List<SeatStatusResponse>,
 )
 
 data class SeatStatusResponse(
-    @Json(name = "id")
     val id: Int,
-    @Json(name = "seatNo")
     val seatNo: String,
-    @Json(name = "status")
     val status: SeatStatus,
 )
 
