bd38534802dbf8f13c90f506c587d6175ba8006a
YUJEONG YUN <yunyj99@naver.com>
2024-02-26 02:56:17 +0900

    Refactor: dev rebase 에러 수정 #205

--- AOS/app/src/main/java/com/softeer/togeduck/di/ServiceModule.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/di/ServiceModule.kt
@@ -3,8 +3,9 @@ package com.softeer.togeduck.di
 import com.softeer.togeduck.data.remote.service.ArticleDetailService
 import com.softeer.togeduck.data.remote.service.HomeService
 import com.softeer.togeduck.data.remote.service.RegionListService
-import com.softeer.togeduck.data.remote.service.UserService
 import com.softeer.togeduck.data.remote.service.ReserveStatusService
+import com.softeer.togeduck.data.remote.service.SeatService
+import com.softeer.togeduck.data.remote.service.UserService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,12 +28,18 @@ class ServiceModule {
         return retrofit.create(HomeService::class.java)
     }
 
-
     @Provides
     @Singleton
     fun provideReserveStatusService(retrofit: Retrofit): ReserveStatusService {
         return retrofit.create(ReserveStatusService::class.java)
     }
+
+    @Provides
+    @Singleton
+    fun provideSeatService(retrofit: Retrofit): SeatService {
+        return retrofit.create(SeatService::class.java)
+    }
+
 
     @Provides
     @Singleton
