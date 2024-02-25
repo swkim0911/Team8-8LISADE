681e5450cacf0db8d13c0096cc7583557750fd42
YUJEONG YUN <yunyj99@naver.com>
2024-02-25 11:33:54 +0900

    Refactor: 좌석 dto 수정 #205
    - numberOfSeats 컬럼은 삭제

--- AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/home/seat/SeatsInfoResponse.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/dto/response/home/seat/SeatsInfoResponse.kt
@@ -3,8 +3,6 @@ package com.softeer.togeduck.data.dto.response.home.seat
 import com.squareup.moshi.Json
 
 data class SeatsInfoResponse(
-    @Json(name = "numberOfSeats")
-    val numberOfSeats: Int,
     @Json(name = "row")
     val row: Int,
     @Json(name = "column")
