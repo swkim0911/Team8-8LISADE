45501506f9e07df702f23bbc1e803114d92c6434
Hymin2 <khm8246@gmail.com>
2024-02-25 23:17:46 +0900

    Feat: UserRemoteDataSource에 FcmToken 전송 함수 추가 #195
    - 구현 내용
        1. UserRemoteDataSource에 FcmToken을 서버에 등록 요청을 하는 함수를 추가했습니다.

--- AOS/app/src/main/java/com/softeer/togeduck/data/remote/datasource/intro/UserRemoteDataSource.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/remote/datasource/intro/UserRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.softeer.togeduck.data.remote.datasource.intro
 
 import com.softeer.togeduck.data.dto.BaseResponse
+import com.softeer.togeduck.data.dto.request.FcmTokenRequest
 import com.softeer.togeduck.data.dto.request.LoginRequest
 import com.softeer.togeduck.data.remote.service.UserService
 import retrofit2.Response
@@ -13,5 +14,11 @@ class UserRemoteDataSource @Inject constructor(
         loginRequest: LoginRequest
     ): Response<String> {
         return userService.login(loginRequest)
+    }
+
+    suspend fun sendFcmToken(
+        fcmTokenRequest: FcmTokenRequest
+    ): Response<String> {
+        return userService.sendFcmToken(fcmTokenRequest)
     }
 }
