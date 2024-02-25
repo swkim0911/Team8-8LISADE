c770a502efe34ca7915a5c11b24d6f36b39b8bba
YUJEONG YUN <yunyj99@naver.com>
2024-02-26 03:22:42 +0900

    Feat: 노선 결제 response, dto, mapper 생성 #205

---
+++ AOS/app/src/main/java/com/softeer/togeduck/data/model/home/seat/SeatPaymentModel.kt
@@ -0,0 +1,15 @@
+package com.softeer.togeduck.data.model.home.seat
+
+data class SeatPaymentModel(
+    val festivalName: String,
+    val festivalAt: String,
+    val departurePlace: String,
+    val departureAt: String,
+    val arrivalPlace: String,
+    val arrivalAt: String,
+    val totalSeats: Int,
+    val reservedSeats: Int,
+    val mySeatNum: Int,
+    val price: Int,
+    val formattedPrice: String,
+)
