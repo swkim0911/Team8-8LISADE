18b16fcf08e88279182ca65050c95561045f24f6
Hymin2 <khm8246@gmail.com>
2024-02-27 05:52:30 +0900

    Fix: 채팅방 리스트 버그 해결 #198
    - 구현 내용
        1. 채팅방 버그 해결

--- BE/src/main/java/com/lisade/togeduck/controller/ChatRoomController.java
+++ BE/src/main/java/com/lisade/togeduck/controller/ChatRoomController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/chat")
+@RequestMapping("/chats")
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
