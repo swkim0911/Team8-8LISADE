b78daf89def773a66254d12d21738c296ae59a0c
Hymin2 <khm8246@gmail.com>
2024-02-27 06:44:37 +0900

    Feat: Fcm 설정 변경 #198
    - 구현 내용
        1. Fcm 설정 변경

--- BE/src/main/java/com/lisade/togeduck/service/FcmService.java
+++ BE/src/main/java/com/lisade/togeduck/service/FcmService.java
@@ -3,7 +3,6 @@ package com.lisade.togeduck.service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
 import com.lisade.togeduck.dto.request.ChatMessageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,11 +15,12 @@ public class FcmService {
         throws FirebaseMessagingException {
         FirebaseMessaging.getInstance().send(
             Message.builder()
-                .setNotification(
-                    Notification.builder()
-                        .setTitle(chatMessageRequest.getRoomName())
-                        .setBody(chatMessageRequest.getMessage())
-                        .build())
+                .putData("roomId", String.valueOf(chatMessageRequest.getRoomId()))
+                .putData("sender", chatMessageRequest.getSender())
+                .putData("roomName", chatMessageRequest.getRoomName())
+                .putData("createdAt", chatMessageRequest.getCreatedAt())
+                .putData("message", chatMessageRequest.getMessage())
+                .putData("action", chatMessageRequest.getAction())
                 .setTopic(String.valueOf(chatMessageRequest.getRoomId()))
                 .build()
         );
