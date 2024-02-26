15bf14d74209b832eaa6fb40e1989a88296fb78f
Hymin2 <khm8246@gmail.com>
2024-02-27 00:15:18 +0900

    Feat: 푸시 알림 전송 방식 변경 #198
    - 구현 내용
        1. 푸시 알림을 FcmToken이 아닌 구독형으로 받을 수 있도록 변경했습니다.

--- BE/src/main/java/com/lisade/togeduck/service/FcmService.java
+++ BE/src/main/java/com/lisade/togeduck/service/FcmService.java
@@ -3,10 +3,7 @@ package com.lisade.togeduck.service;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
-import com.lisade.togeduck.cache.service.FcmTokenCacheService;
-import com.lisade.togeduck.dto.request.FcmTokenRequest;
-import com.lisade.togeduck.entity.User;
+import com.lisade.togeduck.constant.FcmType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,22 +12,17 @@ import org.springframework.stereotype.Service;
 public class FcmService {
 
     private final FirebaseMessaging firebaseMessaging;
-    private final FcmTokenCacheService fcmTokenCacheService;
 
-    public void save(User user, FcmTokenRequest fcmTokenRequest) {
-        fcmTokenCacheService.save(user.getNickname(), fcmTokenRequest.getFcmToken());
-    }
-
-    public void sendNotification(String title, String message, String fcmToken)
+    public void sendNotification(String title, String sender, String message, FcmType fcmType,
+        String id)
         throws FirebaseMessagingException {
         firebaseMessaging.send(
             Message.builder()
-                .setNotification(
-                    Notification.builder()
-                        .setTitle(title)
-                        .setBody(message)
-                        .build()
-                ).setToken(fcmToken)
+                .putData("title", title)
+                .putData("sender", sender)
+                .putData("message", message)
+                .putData("type", fcmType.getType())
+                .setTopic(id)
                 .build()
         );
     }
