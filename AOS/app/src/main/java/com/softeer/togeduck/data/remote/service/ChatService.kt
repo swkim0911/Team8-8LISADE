29d3110bb19ba82aebbeefdcb5a7df082965bd0b
Hymin2 <khm8246@gmail.com>
2024-02-27 05:54:32 +0900

    Feat: 채팅방 리스트 가져오기 uri 변경#195
    - 구현 내용
        1. 채팅방 리스트 가져오기 uri 변경

--- AOS/app/src/main/java/com/softeer/togeduck/data/remote/service/ChatService.kt
+++ AOS/app/src/main/java/com/softeer/togeduck/data/remote/service/ChatService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ChatService {
-    @GET("/chat/rooms")
+    @GET("/chats/rooms")
     suspend fun getChatRoomList(): Response<List<ChatRoomListResponse>>
 }
