fdad35c45f0f627be50f9e6abda6b0386894c057
swkim0911 <kimsungwon364@gmail.com>
2024-03-04 18:00:39 +0900

    Test: 사용자 회원가입 테스트 작성 성공적으로 회원가입할 경우 테스트 작성

--- BE/src/test/java/com/lisade/togeduck/controller/UserControllerTest.java
+++ BE/src/test/java/com/lisade/togeduck/controller/UserControllerTest.java
@@ -3,21 +3,30 @@ package com.lisade.togeduck.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lisade.togeduck.dto.request.SignUpRequest;
+import com.lisade.togeduck.global.handler.ApiResponseHandler;
+import com.lisade.togeduck.global.handler.GlobalExceptionHandler;
 import com.lisade.togeduck.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(SpringExtension.class)
 @ExtendWith(MockitoExtension.class)
@@ -31,8 +40,23 @@ class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    @Mock
+    private GlobalExceptionHandler globalExceptionHandler;
+
+    @Mock
+    private ApiResponseHandler apiResponseHandler;
+
+    @BeforeEach
+    void setUp() {
+        UserController userController = new UserController(userService);
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(userController)
+                .setControllerAdvice(globalExceptionHandler, apiResponseHandler)
+                .build();
+    }
+
     @Test
-    @DisplayName("회원가입 확인")
+    @DisplayName("정상 회원가입 성공")
     void joinUser() throws Exception {
         //given
         SignUpRequest signUpRequest = SignUpRequest.builder()
@@ -43,8 +67,11 @@ class UserControllerTest {
         //when & then
         when(userService.join(any())).thenReturn(1L);
 
-        mockMvc.perform(post("/users")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(new ObjectMapper().writeValueAsString(signUpRequest)));
+        MvcResult mvcResult = mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(signUpRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("1"))
+                .andReturn();
     }
 }
